73ae1147461ed0606ea32a053f99aa746935a002
Marc Davies <marcdaviesriot@gmail.com>
2019-10-29 13:24:44 +0100

    add beliefs page

---
+++ beliefs.php
@@ -0,0 +1,16 @@
+<?php $page_title = "Celebrations"; ?>
+
+<?php include("header.php"); ?>
+<?php include("navigation.php"); ?>
+
+<div class="container">
+    <h1 class="my-4"><?php echo $page_title; ?></h1>
+    <h3>Content for this page:</h3>
+    <ul>
+        <li>Equity Statement</li>
+        <li>Anti-Discrimination Statement</li>
+    </ul>
+    <a href="index.php" class="btn btn-primary">&larr; Back</a>
+</div>
+
+<?php include("footer.php"); ?>
