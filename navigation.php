bacaac43208de4596b44c5c202735fdecdeacf02
Marc Davies <marcdaviesriot@gmail.com>
2019-10-29 12:09:02 +0100

    remove multiple dropdown navigation

--- navigation.php
+++ navigation.php
@@ -1,157 +1,28 @@
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="../../index.php">
-            <img src="../../images/logo.png" alt="Multnomah Farm County Bureau" />
-        </a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <!-- Level one dropdown -->
-                <li class="nav-item dropdown">
-                    <a id="dropdownMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">About</a>
-                    <ul aria-labelledby="dropdownMenu1" class="dropdown-menu border-0 shadow">
-                        <li><a class="dropdown-item" href="mission.php">Mission</a></li>
-                        <li><a class="dropdown-item" href="vision.php">Vision</a></li>
-                        <li><a class="dropdown-item" href="values.php">Values</a></li>
-                        <li><a class="dropdown-item" href="farming-in-multnomah-county.php">Farming in Multnomah County</a></li>
-                        <!-- Level two dropdown-->
-                        <li class="dropdown-submenu">
-                            <a id="dropdownMenu1a" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">People</a>
-                            <ul aria-labelledby="dropdownMenu1a" class="dropdown-menu border-0 shadow">
-                                <li>
-                                    <a href="members.php" class="dropdown-item">Members</a>
-                                    <a href="board-of-directors.php" class="dropdown-item">Board of Directors</a>
-                                    <a href="staff.php" class="dropdown-item">Staff</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown-submenu">
-                            <a id="dropdownMenu1b" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Documents</a>
-                            <ul aria-labelledby="dropdownMenu1b" class="dropdown-menu border-0 shadow">
-                                <li>
-                                    <a href="articles-of-incorporation.php" class="dropdown-item">Articles of Incorporation</a>
-                                    <a href="by-laws.php" class="dropdown-item">By-laws</a>
-                                    <a href="conflict-of-interest.php" class="dropdown-item">Conflict of Interest</a>
-                                    <a href="plans.php" class="dropdown-item">Plans</a>
-                                    <a href="budgets.php" class="dropdown-item">Budgets</a>
-                                    <a href="reports.php" class="dropdown-item">Reports</a>
-                                    <a href="board-materials.php" class="dropdown-item">Board Materials</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <!-- End Level two -->
-                    </ul>
-                </li>
-                <!-- End Level one -->
-                <!-- Level one dropdown -->
-                <li class="nav-item dropdown">
-                    <a id="dropdownMenu2" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Strategies</a>
-                    <ul aria-labelledby="dropdownMenu2" class="dropdown-menu border-0 shadow" style="width: 300px">
-                        <!-- Level two dropdown-->
-                        <li class="dropdown-submenu">
-                            <a id="dropdownMenu2a" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Training</a>
-                            <ul aria-labelledby="dropdownMenu2a" class="dropdown-menu border-0 shadow">
-                                <li>
-                                    <a href="training-index.php" class="dropdown-item">Goals, Metrics & Activities</a>
-                                    <a href="scholarships.php" class="dropdown-item">Scholarships</a>
-                                    <a href="conferences.php" class="dropdown-item">Conferences</a>
-                                    <a href="learning-events.php" class="dropdown-item">Learning Events</a>
-                                    <a href="workshops.php" class="dropdown-item">Workshops</a>
-                                    <a href="apprenticeships.php" class="dropdown-item">Apprenticeships</a>
-                                    <a href="resources.php" class="dropdown-item">Resources</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown-submenu">
-                            <a id="dropdownMenu2b" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Advocacy</a>
-                            <ul aria-labelledby="dropdownMenu2b" class="dropdown-menu border-0 shadow">
-                                <li>
-                                    <a href="advocacy-index.php" class="dropdown-item">Goals, Metrics & Activities</a>
-                                    <a href="elected-officials.php" class="dropdown-item">Elected Officials</a>
-                                    <a href="local-state-and-federal-laws-and-rules.php" class="dropdown-item">Local, State & Federal Laws and Rules</a>
-                                    <a href="voter-education.php" class="dropdown-item">Voter Education</a>
-                                    <a href="advisory-committees.php" class="dropdown-item">Advisory Committees</a>
-                                    <a href="organizational-support.php" class="dropdown-item">Organizational Support</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown-submenu">
-                            <a id="dropdownMenu2c" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Networking</a>
-                            <ul aria-labelledby="dropdownMenu2c" class="dropdown-menu border-0 shadow">
-                                <li>
-                                    <a href="networking-index.php" class="dropdown-item">Goals, Metrics & Activities</a>
-                                    <a href="newsletters.php" class="dropdown-item">Newsletters</a>
-                                    <a href="social-media.php" class="dropdown-item">Social Media</a>
-                                    <a href="farm-stands-and-agritourism.php" class="dropdown-item">Farm Stands & Agritourism</a>
-                                    <a href="social-outings.php" class="dropdown-item">Social Outings</a>
-                                    <a href="celebrations.php" class="dropdown-item">Celebrations</a>
-                                    <a href="board-meetings.php" class="dropdown-item">Board Meetings</a>
-                                    <a href="calendar-of-events.php" class="dropdown-item">Calendar of Events</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown-submenu">
-                            <a id="dropdownMenu2d" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">On-farm & Community Projects</a>
-                            <ul aria-labelledby="dropdownMenu2d" class="dropdown-menu border-0 shadow">
-                                <li>
-                                    <a href="on-farm-and-community-projects-index.php" class="dropdown-item">Goals, Metrics & Activities</a>
-                                    <a href="ask-a-farmer.php" class="dropdown-item">Ask a Farmer</a>
-                                    <a href="legal-assistance.php" class="dropdown-item">Legal Assistance</a>
-                                    <a href="ronald-mcdonald-house.php" class="dropdown-item">Ronald McDonald House</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <!-- End Level two -->
-                    </ul>
-                </li>
-                <!-- End Level one -->
-                <li class="nav-item"><a href="benefits.php" class="nav-link">Benefits</a></li>
-                <!-- Level one dropdown -->
-                <li class="nav-item dropdown">
-                    <a id="dropdownMenu3" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Join Us</a>
-                    <ul aria-labelledby="dropdownMenu3" class="dropdown-menu border-0 shadow">
-                        <li>
-                            <a href="join-us-index.php" class="dropdown-item">General Information</a>
-                            <a href="membership.php" class="dropdown-item">Membership</a>
-                            <a href="board-of-directors-join.php" class="dropdown-item">Board of Directors</a>
-                            <a href="contracting.php" class="dropdown-item">Contracting</a>
-                            <a href="volunteering.php" class="dropdown-item">Volunteering</a>
-                            <a href="training.php" class="dropdown-item">Training</a>
-                            <a href="outings.php" class="dropdown-item">Outings</a>
-                            <a href="celebrations-join.php" class="dropdown-item">Celebrations</a>
-                            <a href="meetings.php" class="dropdown-item">Meetings</a>
-                            <a href="calendar-of-events-join.php" class="dropdown-item">Calendar of Events</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- End Level one -->
-                <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
-                <li class="nav-item dropdown">
-                    <a id="dropdownMenu3" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Page Types</a>
-                    <ul aria-labelledby="dropdownMenu3" class="dropdown-menu border-0 shadow">
-                        <li>
-                            <a href="404.php" class="dropdown-item">404</a>
-                            <a href="about.php" class="dropdown-item">About</a>
-                            <a href="blog-home-1.php" class="dropdown-item">Blog 1</a>
-                            <a href="blog-home-2.php" class="dropdown-item">Blog 2</a>
-                            <a href="blog-post.php" class="dropdown-item">Blog Post</a>
-                            <a href="faq.php" class="dropdown-item">FAQ</a>
-                            <a href="portfolio-1-col.php" class="dropdown-item">Portfolio 1</a>
-                            <a href="portfolio-2-col.php" class="dropdown-item">Portfolio 2</a>
-                            <a href="portfolio-3-col.php" class="dropdown-item">Portfolio 3</a>
-                            <a href="portfolio-4-col.php" class="dropdown-item">Portfolio 4</a>
-                            <a href="portfolio-item.php" class="dropdown-item">Portfolio Item</a>
-                            <a href="pricing.php" class="dropdown-item">Pricing</a>
-                            <a href="services.php" class="dropdown-item">Services</a>
-                            <a href="sidebar.php" class="dropdown-item">Sidebar</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- End Level one -->
-            </ul>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-between fixed-top">
+    <a class="navbar-brand" href="../../index.php">
+        <img src="../../images/logo.png" alt="Multnomah Farm County Bureau" />
+    </a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/about.php">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about.php">Strategies</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about.php">Benefits</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about.php">Join Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about.php">Contact</a>
+            </li>
+        </ul>
     </div>
 </nav>
+
