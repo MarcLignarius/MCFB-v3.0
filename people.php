f21dfa36a7eb948bc8ec35107f205faf6bdd46f8
Marc Davies <marcdaviesriot@gmail.com>
2019-11-03 13:21:11 +0100

    create people page

---
+++ people.php
@@ -0,0 +1,60 @@
+<?php $page_title = "People"; ?>
+
+<?php include("header.php"); ?>
+<?php include("navigation.php"); ?>
+
+<div class="container-fluid" style="margin-top: 30px">
+    <h1 class="text-center">People</h1>
+    <div class="row">
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Members</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="members.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Board Of Directors</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="board-of-directors.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Staff</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="staff.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("statement.php"); ?>
+<?php include("footer.php"); ?>
