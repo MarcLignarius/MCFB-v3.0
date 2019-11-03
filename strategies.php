6316cc06f91ed5df53c2ed2321a7f8e715e150d5
Marc Davies <marcdaviesriot@gmail.com>
2019-11-03 16:49:50 +0100

    add content to strategies page

---
+++ strategies.php
@@ -0,0 +1,80 @@
+<?php $page_title = "Strategies"; ?>
+
+<?php include("header.php"); ?>
+<?php include("navigation.php"); ?>
+
+<div class="container-fluid">
+    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <div class="row">
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Training</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="training-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Advocacy</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="advocacy-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Networking</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="networking-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">On-farm & Community Projects</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="on-farm-and-community-projects.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class="text-center">
+        <a href="index.php" class="btn btn-primary">&larr; Back</a>
+    </p>
+    <?php include("statement.php"); ?>
+</div>
+
+<?php include("footer.php"); ?>
