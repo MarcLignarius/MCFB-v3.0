d05ca97c3149be0e1bbd9ddcbb9b421f4017dc5a
Marc Davies <marcdaviesriot@gmail.com>
2019-11-03 15:45:34 +0100

    update on farm and community projects index page

--- on-farm-and-community-projects-index.php
+++ on-farm-and-community-projects-index.php
@@ -3,27 +3,71 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
-<div class="container">
-    <h1 class="my-4"><?php echo $page_title; ?></h1>
-    <h3>Content for this page:</h3>
-    <h5>Goal:</h5>
+<div class="container-fluid">
+    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <h3>Goal:</h3>
     <p>
-        Identify the greatest needs for local on-farm assistance and ways in which MCFB 
+        Identify the greatest needs for local on-farm assistance and ways in which MCFB
         can help meet those needs; establish and implement relevant program.
     </p>
-    <h5>Metrics:</h5>
+    <h3>Metrics:</h3>
     <ul>
         <li>2019 - 2020: Research regarding on-farm needs, opportunities, and providers completed</li>
         <li>2021 - 2022: Launch of on-farm trials</li>
         <li>2023 - 2025: Implementation of more regularized program for on-farm assistance</li>
     </ul>
-    <h5>Current and Ongoing Activities:</h5>
-    <ul>
-        <li><a href="ask-a-farmer.php" class="btn btn-link">Ask a Farmer &rarr;</a></li>
-        <li><a href="legal-assistance.php" class="btn btn-link">Legal Assisstance &rarr;</a></li>
-        <li><a href="ronald-mcdonald-house.php" class="btn btn-link">Ronald McDonald House &rarr;</a></li>
-    </ul>
-    <p>
+    <h3>Current and Ongoing Activities:</h3>
+    <div class="row">
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Ask A Farmer</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="ask-a-farmer.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Legal Assistance</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="legal-assistance.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Ronald McDonald House</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="ronald-mcdonald-house.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class="text-center">
         <a href="mission.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
     </p>
