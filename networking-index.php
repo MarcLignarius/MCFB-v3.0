82893d81db5af519fd355613719f5316747e5621
Marc Davies <marcdaviesriot@gmail.com>
2019-11-03 15:31:06 +0100

    update networking-index page

--- networking-index.php
+++ networking-index.php
@@ -3,34 +3,141 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
-<div class="container">
-    <h1 class="my-4"><?php echo $page_title; ?></h1>
-    <h3>Content for this page:</h3>
-    <h5>Goal:</h5>
+<div class="container-fluid">
+    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <h3>Goal:</h3>
     <p>
-        Establish a professional network of local farmers through which formal and informal 
+        Establish a professional network of local farmers through which formal and informal
         communications and exchanges occur to help farmers address issues they are facing.
     </p>
-    <h5>Metrics:</h5>
+    <h3>Metrics:</h3>
     <ul>
         <li>2019: Research regarding professional networking options completed</li>
         <li>2020: Program of work for professional network development established</li>
         <li>2020 - 2025: Implementation of program of work begun</li>
     </ul>
-    <h5>Current and Ongoing Activities:</h5>
-    <ul>
-        <li><a href="newsletters.php" class="btn btn-link">Newsletters &rarr;</a></li>
-        <li><a href="social-media.php" class="btn btn-link">Social Media &rarr;</a></li>
-        <li><a href="farm-stands-and-agritourism.php" class="btn btn-link">Farm Stands & Agritourism &rarr;</a></li>
-        <li><a href="social-outings.php" class="btn btn-link">Social Outings &rarr;</a></li>
-        <li><a href="celebrations.php" class="btn btn-link">Celebrations &rarr;</a></li>
-        <li><a href="board-meetings.php" class="btn btn-link">Board Meetings &rarr;</a></li>
-        <li><a href="calendar-of-events.php" class="btn btn-link">Calendar of Events &rarr;</a></li>
-    </ul>
-    <p>
+    <h3>Current and Ongoing Activities:</h3>
+    <div class="row">
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Newsletters</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="newsletters.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Social Media</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="social-media.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Farm Stands & Agritourism</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="farm-stands-and-agritourism.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Social Outings</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="social-outings.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Celebrations</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="celebrations.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Board Meetings</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="board-meetings.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Calendar Of Events</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="calendar-of-events.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class="text-center">
         <a href="mission.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
     </p>
+    <?php include("statement.php"); ?>
 </div>
 
 <?php include("footer.php"); ?>
