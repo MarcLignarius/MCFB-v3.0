021466914a77a53454c730c5cdb0a75957241430
Marc Davies <marcdaviesriot@gmail.com>
2019-11-03 14:03:23 +0100

    update mission page

--- mission.php
+++ mission.php
@@ -4,20 +4,76 @@
 <?php include("navigation.php"); ?>
 
 <div class="container-fluid" style="margin-top: 30px">
-    <h1 class="my-4"><?php echo $page_title; ?></h1>
-    <h3>Content for this page:</h3>
-    <p>
-        MCFB’s mission is to, Help local farmers be successful. We do this through four strategies:
+    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p class="text-center">
+        MCFB’s mission is to help local farmers be successful. We do this through four strategies:
     </p>
-    <ul>
-        <li><a href="training-index.php" class="btn btn-link">Training &rarr;</a></li>
-        <li><a href="advocacy-index.php" class="btn btn-link">Advocacy &rarr;</a></li>
-        <li><a href="networking-index.php" class="btn btn-link">Networking &rarr;</a></li>
-        <li><a href="on-farm-and-community-projects-index.php" class="btn btn-link">On-farm & Community Projects &rarr;</a></li>
-    </ul>
-    <p>
-        <a href="about.php" class="btn btn-primary">&larr; Back</a>
-    </p>
+    <div class="row">
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Training</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="training-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Advocacy</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="advocacy-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">Networking</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="networking-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 mb-3">
+            <div class="card h-100 text-center" style="border-radius: 0px; border: none">
+                <div class="image-text-container">
+                    <img style="width: 100%" src="../images/placeholder.png" alt="">
+                    <h2 class="card-title">On-farm & Community Projects</h2>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
+                    </p>
+                </div>
+                <div>
+                    <a href="on-farm-and-community-projects-index.php" class="btn btn-link">Learn More &rarr;</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include("statement.php"); ?>
 </div>
 
