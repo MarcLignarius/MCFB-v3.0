e7cda1d5480e1bac5f3a1085840b055c61ba56d3
Marc Davies <marcdaviesriot@gmail.com>
2019-11-03 16:03:48 +0100

    update layout and links for pages nested under on farm and community projects index page

--- ask-a-farmer.php
+++ ask-a-farmer.php
@@ -6,14 +6,17 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
-<div class="container">
-    <h1 class="my-4"><?php echo $page_title; ?></h1>
+<div class="container-fluid">
+    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <h3>Content for this page:</h3>
     <ul>
         <li>TBD</li>
     </ul>
-    <a href="on-farm-and-community-projects-index.php" class="btn btn-primary">&larr; Back</a>
-    <a href="index.php" class="btn btn-primary">Home</a>
+    <p class="text-center">
+        <a href="on-farm-and-community-projects-index.php" class="btn btn-primary">&larr; Back</a>
+        <a href="index.php" class="btn btn-primary">Home</a>
+    </p>
+    <?php include("statement.php"); ?>
 </div>
 
 <?php include("footer.php"); ?>
