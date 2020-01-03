952b4720a5e8a5d0af730e826bb514bad8207c9a
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 20:09:02 -0800

    add client copy for purpose page

--- purpose.php
+++ purpose.php
@@ -6,16 +6,19 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p class="text-center">
-        Our Vision: Our local farming communities are economically and environmentally vibrant, healthy, and sustainable, providing quality livelihoods and products both near and far.
+        MCFB’s Purpose is represented by our vision, mission, and values. These represent what we stand for and work towards. The vision, mission, and values guide all of our actions. Opportunities put before the board for decisions will be evaluated based on well the intended outcomes support our vision, mission, and values. Our vision, mission, and values are below.
     </p>
     <p class="text-center">
-        Our Mission: We help local farmers be successful.
+        MCFB’s Vision: Farmers experience high quality livelihoods by growing world class products that ultimately support vibrant, healthy, and sustainable farms and communities.
     </p>
     <p class="text-center">
-        Our Values: We believe that greater human diversity, equity, and inclusion within our organization and our local farms will create healthier, happier, and more prosperous farming communities. To that end, we will strive to cultivate diversity, advance equity, and practice inclusion in all of our work.
+        MCFB's Mission: We help local farmers be successful.
     </p>
     <p class="text-center">
-        Our Approach: <a href="purpose-our-approach.php" class="btn btn-link">Link to page &rarr;</a>
+        MCFB's Values: MCFB believes that healthier, happier, and more prosperous farming communities can be supported through greater human diversity, equity, and inclusion within our local farms and our organization.
+    </p>
+    <p class="text-center">
+        MCFB's Approach: Click <a href="purpose-our-approach.php"> HERE </a> to learn more about the actions we use to implement our Purpose.
     </p>
     <p class="text-center">
         <a href="about.php" class="btn btn-primary">&larr; Back</a>
@@ -24,73 +27,3 @@
 </div>
 
 <?php include("footer.php"); ?>
-
-<!-- <p class="text-center">
-    MCFB’s mission is to help local farmers be successful. We do this through four strategies:
-</p>
-<div class="row">
-    <div class="col-xl-3 col-md-6 mb-3">
-        <div class="card h-100 text-center">
-            <div class="image-text-container">
-                <img class="card-image" src="../images/placeholder.png" alt="">
-                <h2 class="card-title">Training</h2>
-            </div>
-            <div class="card-body">
-                <p class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                </p>
-            </div>
-            <div>
-                <a href="sharing-knowledge-with-farmers.php" class="btn btn-link">Learn More &rarr;</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-md-6 mb-3">
-        <div class="card h-100 text-center">
-            <div class="image-text-container">
-                <img class="card-image" src="../images/placeholder.png" alt="">
-                <h2 class="card-title">Advocacy</h2>
-            </div>
-            <div class="card-body">
-                <p class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                </p>
-            </div>
-            <div>
-                <a href="representing-farmers.php" class="btn btn-link">Learn More &rarr;</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-md-6 mb-3">
-        <div class="card h-100 text-center">
-            <div class="image-text-container">
-                <img class="card-image" src="../images/placeholder.png" alt="">
-                <h2 class="card-title">Networking</h2>
-            </div>
-            <div class="card-body">
-                <p class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                </p>
-            </div>
-            <div>
-                <a href="connecting-farmers.php" class="btn btn-link">Learn More &rarr;</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-md-6 mb-3">
-        <div class="card h-100 text-center">
-            <div class="image-text-container">
-                <img class="card-image" src="../images/placeholder.png" alt="">
-                <h2 class="card-title">On-farm & Community Projects</h2>
-            </div>
-            <div class="card-body">
-                <p class="card-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                </p>
-            </div>
-            <div>
-                <a href="on-farm-and-community-projects-index.php" class="btn btn-link">Learn More &rarr;</a>
-            </div>
-        </div>
-    </div>
-</div> -->
