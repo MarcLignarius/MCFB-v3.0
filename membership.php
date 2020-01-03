0e20ce69d4d1b289140e4099f2da26a61bd419fd
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 20:32:43 -0800

    add client copy for our membership  page

--- membership.php
+++ membership.php
@@ -36,14 +36,37 @@
         </div>
     </div>
     <p>
-        Farmers with $2,500 or more per year in gross income qualify as voting members while farmers
-        with less income and non-farmers qualify as supporting members. All Country Financial
-        customers are automatically associate members.
+        Farmers and ranchers with $2,500 or more/year in gross farming/ranching income qualify as voting members. Voting members may:
+    </p>
+    <ul>
+        <li>Attend all county board meetings, annual meetings, events, and outings</li>
+        <li>Vote annually at the annual county meeting for board directors and officers</li>
+        <li>Attend the annual state and national conventions</li>
+        <li>Run for county and state boards and officer positions</li>
+        <li>Represent the county as a voting delegate at the annual state convention</li>
+        <li>Receive all of the benefits from the state farm bureau</li>
+    </ul>
+    <p>
+        Click <a href="benefits.php" target="blank">HERE</a> to learn more about benefits.
     </p>
     <p>
-        Membership can be attained by completing a hardcopy membership packet, by completing the OFB's
+        Farmers and ranchers with less than $2,500/year in gross farming/ranching income and non-farmers qualify as supporting members. Supporting members may:
+    </p>
+    <ul>
+        <li>Attend all county board meetings, annual meetings, events, and outings</li>
+        <li>Attend the annual state and national conventions</li>
+        <li>Receive all of the benefits from the state farm bureau</li>
+    </ul>
+    <p>
+        Click <a href="benefits.php" target="blank">HERE</a> to learn more about benefits.
+    </p>
+    <p>
+        Membership can be attained by completing a hard copy membership packet, by completing the OFB's
         <a href="https://oregonfb.org/join/" target="blank">online application</a> or by signing up for OFB
         through Country Financial. For more information, <a href="contact.php">contact MCFB</a>.
+    </p>
+    <p>
+        Through a special arrangement with Country Financial, all Country Financial customers are automatically associate members. Only Country Financial customers can be associate members. Associate members support the MCFB and OFB financially with annual dues and may attend meetings, events, and outings with approval from the MCFB Board President or Executive Director.
     </p>
     <p class="text-center">
         <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
