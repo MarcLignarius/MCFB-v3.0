0d86f758e0ed9157c5506b118e3fb32136bde536
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 22:04:05 -0800

    add client copy for board meetings page

--- board-meetings.php
+++ board-meetings.php
@@ -5,8 +5,22 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>
+        MCFB Board monthly meetings and the annual meeting are normally held as per the schedule below. Notices, locations, and changes to this schedule are provided through newsletters, mailers, and on the MCFB Facebook page. Voting and Supporting members are always welcome to attend any of these meetings. Associate members, members of the public, and partner representatives should <a href="contact.php">contact us</a> if you would like to attend a meeting.
+    </p>
     <ul>
-        <li>TBD</li>
+        <li>January, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
+        <li>February, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
+        <li>March, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
+        <li>April, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
+        <li>May, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
+        <li>June: off for farming</li>
+        <li>July: off for farming</li>
+        <li>August: off for farming</li>
+        <li>September, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
+        <li>October, 3rd Thursday at 7:00 PM:Monthly  Board Meeting</li>
+        <li>November, Day/Time/Place TBD: Annual Board Meeting</li>
+        <li>December, Day/Time/Place TBD: Annual End-of-Year Holiday Party</li>
     </ul>
     <p class="text-center">
         <a href="connecting-farmers.php" class="btn btn-primary">&larr; Back</a>
