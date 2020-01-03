a587e31335d6478b92f01040ea3f36ad2648aab8
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:00:25 -0800

    add client copy for documents page

--- documents.php
+++ documents.php
@@ -3,16 +3,17 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
-<div class="container-fluid" >
+<div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p class="text-center">
+        MCFB foundational, guiding, annual, and Board-related documents can be found here. If you are looking for an MCFB document that is not on this page, please contact us.
+    </p>
     <ul>
         <li><a href="articles-of-incorporation.php">Articles of Incorporation</a></li>
         <li><a href="by-laws.php">By-laws</a></li>
-        <li><a href="conflict-of-interest.php">Conflict of Interest</a></li>
-        <li><a href="plans.php">Plans</a></li>
-        <li><a href="budgets.php">Budgets</a></li>
-        <li><a href="reports.php">Reports</a></li>
-        <li><a href="board-materials.php">Board Materials</a></li>
+        <li><a href="business-plan.php">Business Plan</a></li>
+        <li><a href="annual-budgets.php">Annual Budgets</a></li>
+        <li><a href="board-materials.php">Board Meetings Agendas & Minutes</a></li>
     </ul>
     <p class="text-center">
         <a href="about.php" class="btn btn-primary">&larr; Back</a>
