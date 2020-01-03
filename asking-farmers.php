6772844a467a77bc867d245f4c8d54d4ef93e665
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:11:49 -0800

    add client copy for asking farmers page

--- asking-farmers.php
+++ asking-farmers.php
@@ -8,9 +8,22 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p class="text-center">
+        People have questions about farming. Whether members of the general public, students, industry representatives, government employees, or elected officials, when farm-related questions come up, people tend to turn to the usual information sources. These information sources often consist of friends, colleagues, books, the Internet, or academic experts. Rarely, though, do people have ready access to real farmers. Real farmers have unique experiences, knowledge, and understanding of land and farming techniques and issues. Farmers, local communities, and our society in general will benefit if people who are seeking to understand farming better have the ability to ask farmers directliy. Given this, MCFB wants to connect people with questions about farming with real farmers. To accomplish this, we will establish the “Ask a Farmer” program.
+    </p>
+    <p>
+        Recent questions posed to our farmers include:
+    </p>
     <ul>
-        <li>TBD</li>
+        <li>How much does local farmland lease for per year?</li>
+        <li>What does a farm business plan look like?</li>
+        <li>What is it like to have a solar field within a farm?</li>
+        <li>How much water is required annually for a single horse?</li>
     </ul>
+    <p>
+        If you have a question that you would like to pose to a local farmer, <a href="contact.php">Contact Us</a>.
+    </p>
+
     <p class="text-center">
         <a href="our-approach.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
