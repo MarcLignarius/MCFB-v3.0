12bbf7e35c857c3725dc9e0a6f3cfef13813c812
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:54:47 -0800

    add client copy for social outings page

--- social-outings.php
+++ social-outings.php
@@ -5,8 +5,19 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>
+        MCFB participates in, co-sponsors, and sometimes organizes and hosts member outings every year. These outings are intended to be social activities that encourage members to establish and maintain friendships with one another (and others) in a casual atmosphere. 
+    </p>
+    <p>
+        If you're an MCFB member and would like to recommend an outing or join a currently scheduled outing, please <a href="contact.php">contact us</a>.
+    </p>
+    <p>
+        Annual outings include:
+    </p>
     <ul>
-        <li>TBD</li>
+        <li>May: Trap Shoot (click <a href="https://oregonfb.org/trapshoot-2/">HERE</a> for more info)</li>
+        <li>July: Mt. Hood Pitch & Put/Golfing (click <a href="https://www.oan.org/events/event_details.asp?legacy=1&id=1213110">HERE</a> for more info)</li>
+        <li>July: Volcanoes Baseball Game (click <a href="https://oregonfb.org/volcanoesgame/">HERE</a> for more info)</li>
     </ul>
     <p class="text-center">
         <a href="connecting-farmers.php" class="btn btn-primary">&larr; Back</a>
