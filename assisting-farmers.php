36aaaed3d1769386a7844a2cb71f15668fa31707
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:16:05 -0800

    add client copy for assisting farmers page

--- assisting-farmers.php
+++ assisting-farmers.php
@@ -5,10 +5,21 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>
+        Farmers are resourceful and creative people, but sometimes they would like assistance solving problems or addressing issues on their land. While there are many local, state, and federal entities that assist farmers in a variety of ways, many farmers would prefer to engage with a local farm group, such as MCFB, to help them solve problems. Given this, MCFB undertakes projects and activities to assist local farmers and communities.
+    </p>
+    <p>
+        Recent efforts to assist our farmers and local communities include:
+    </p>
     <ul>
-        <li>Legal Assistance</li>
-        <li>Ronald McDonald House</li>
+        <li>Financial contributions for legal aid to defend a farmer against administrative actions.</li>
+        <li>Financial contributions to the Ronald McDonald House.</li>
+        <li>Research into the Portland Bureau of Environmental Services Water Submeter Program.</li>
+        <li>Advice regarding farm equipment export for developing countries.</li>
     </ul>
+    <p>
+        If you are a local farmer and would like MCFB assistance with an issue, problem, or concern, <a href="contact.php">contact us</a>.
+    </p>
     <p class="text-center">
         <a href="our-approach.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
