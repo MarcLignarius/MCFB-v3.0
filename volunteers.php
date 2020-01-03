640fed63b59d103f595675ac2380a6ad5e2a40ee
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 20:54:59 -0800

    add client copy for volunteers page

--- volunteers.php
+++ volunteers.php
@@ -5,12 +5,24 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p class="text-center">
+        MCFB occassionally benefits from the time and expertise of volunteers.Volunteers could lead or help with activities such as website development and maintenance, social media management, and event hosting. If you would like to volunteer or have a volunteer idea, please contact us.
+    </p>
+    <p>
+        Current MCFB Volunteers:
+    </p>
     <ul>
-        <li>TBD</li>
+        <li>Marc Davies</li>
+    </ul>
+    <p>
+        Current Volunteer Opportunities:
+    </p>
+    <ul>
+        <li>Social Media Volunteer. <a href="contact.php">Contact Us</a> if interested.</li>
     </ul>
     <p class="text-center">
-        <a href="our-people.php" class="btn btn-primary">&larr; Back</a>   
-        <a href="index.php" class="btn btn-primary">Home</a>  
+        <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
+        <a href="index.php" class="btn btn-primary">Home</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
