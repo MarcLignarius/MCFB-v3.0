b0ac430c1da40235fe21d01c3ea02b72aafe6a3b
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:59:06 -0800

    update copy

--- celebrations.php
+++ celebrations.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p>
-        MCFB hosts one major celebration -- the end of year holiday party -- each year in December. Notices of the date, time, and place of the annual holiday party are sent to Voting and Supporting members through newsletters and mailers. A notice is also posted on the MCFB Facebook Page. Other celebrations may be scheduled and noticed on a periodic basis. 
+        MCFB hosts one major celebration - the end of year holiday party - each year in December. Notices of the date, time, and place of the annual holiday party are sent to Voting and Supporting members through newsletters and mailers. A notice is also posted on the MCFB Facebook Page. Other celebrations may be scheduled and noticed on a periodic basis. 
     </p>
     <p>
         If you're an MCFB member and would like to recommend a celebration or join the end of year holiday party, please <a href="contact.php">contact us</a>.
