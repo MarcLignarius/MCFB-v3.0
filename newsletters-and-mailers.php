27b5744019e6816a1dbef6ba680978f146f26840
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:24:12 -0800

    add client copy for newsletters and mailers page

---
+++ newsletters-and-mailers.php
@@ -0,0 +1,29 @@
+<?php $page_title = "Newsletters and Mailers"; ?>
+
+<?php include("header.php"); ?>
+<?php include("navigation.php"); ?>
+
+<div class="container-fluid">
+    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>
+        MCFB uses periodic newsletters and mailers to keep its members up to date regarding meetings, events, outings, and other networking opportunities and newsworthy issues. Newsletters and mailers are sent to Voting and Supporting members by email and postal service. Associate members can opt-in to receive newsletters and mailers by Contacting Us. Previous newsletters and mailers are archived below:
+    </p>
+    <ul>
+        <li>MCFB November 2019 mailer</li>
+        <li>MCFB October 2019 mailer</li>
+        <li>MCFB September/October 2018 newsletter</li>
+        <li>MCFB April 2018 newsletter</li>
+        <li>MCFB March 2018 newsletter</li>
+        <li>MCFB February 2018 newsletter</li>
+        <li>MCFB February 2017 newsletter</li>
+        <li>MCFB September 2016 newsletter</li>
+        <li>MCFB March 2016 newsletter</li>
+    </ul>
+    <p class="text-center">
+        <a href="connecting-farmers.php" class="btn btn-primary">&larr; Back</a>
+        <a href="index.php" class="btn btn-primary">Home</a>
+    </p>
+    <?php include("statement.php"); ?>
+</div>
+
+<?php include("footer.php"); ?>
