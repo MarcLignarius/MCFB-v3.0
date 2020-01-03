c0fb655f522732c36477826756f384431ab8eacd
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:27:37 -0800

    add client copy for social media page

--- social-media.php
+++ social-media.php
@@ -5,8 +5,12 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>
+    MCFB uses social media as an informal means to communicate on a daily and weekly basis with MCFB members and the general public. Please like, share, and follow us on the social media platforms below:
+    </p>
     <ul>
-        <li>TBD</li>
+        <li><a href="https://www.facebook.com/multnomahcountyfarmbureau/">Facebook</a></li>
+        <li><a href="https://www.linkedin.com/company/multnomah-county-farm-bureau">LinkedIn</a></li>
     </ul>
     <p class="text-center">
         <a href="connecting-farmers.php" class="btn btn-primary">&larr; Back</a>
