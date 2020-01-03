7d3834abe04fde07f410baef1c4acd5b4cfce4b0
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 20:49:15 -0800

    add client copy for contractors page

--- contractors.php
+++ contractors.php
@@ -5,12 +5,26 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
-    <ul>
-        <li>TBD</li>
-    </ul>
     <p class="text-center">
-        <a href="our-people.php" class="btn btn-primary">&larr; Back</a>   
-        <a href="index.php" class="btn btn-primary">Home</a>  
+        MCFB does not have employees. Currently, MCFB contracts for services related to the executive director/secretary and treasurer.
+    </p>
+    <p>
+        Current MCFB Contractors:
+    </p>
+    <ul>
+        <li>Jay Udelhoven, Executive Director/Secretary</li>
+        <li>Deniece Tucker, Treasurer</li>
+    </ul>
+    <p>
+        Current Contract Opportunities:
+    </p>
+    <ul>
+        <li>None at this time. <a href="contact.php">Contact Us</a> if you have a suggestion.</li>
+    </ul>
+    </p>
+    <p class="text-center">
+        <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
+        <a href="index.php" class="btn btn-primary">Home</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
