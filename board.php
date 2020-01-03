f6378bc6ef4eff649273fd8feb5d3b64ee30a89a
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 20:42:57 -0800

    add client copy for board page

--- board.php
+++ board.php
@@ -5,12 +5,25 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p class="text-center">
+        MCFB's Board of Directors must include a President and Secretary as Board Officers, and no less than one additional Board Director. The Board of Directors may choose to elect a Vice-President and Treasurer as additional Board Officers, and up to eight additional Board Directors. In addition, if there is a county Women's Committee and/or a Young Farmers and Ranchers Committee, the Chair of each committee shall be a voting member of the Board of Directors.
+        Board Officers are elected each year at the annual MCFB meeting. Other Board Directors, also elected at the MCFB annual meeting, serve staggered terms of two years. Only full MCFB Voting Members can serve on the Board of Directors.
+    </p>
+    <p>
+        Current MCFB Board Directors:
+    </p>
     <ul>
-        <li>TBD</li>
+        <li>Larry Bailey, President</li>
+        <li>Vacant, Secretary</li>
+        <li>Deniece Tucker, Treasurer</li>
+        <li>Al Garre, Board Director</li>
+        <li>Helen Bushue, Board Director</li>
+        <li>Joe Rossi, Board Director</li>
+        <li>John Bergan, Board Director</li>
     </ul>
     <p class="text-center">
-        <a href="our-people.php" class="btn btn-primary">&larr; Back</a>   
-        <a href="index.php" class="btn btn-primary">Home</a>  
+        <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
+        <a href="index.php" class="btn btn-primary">Home</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
