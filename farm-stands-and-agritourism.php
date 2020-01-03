bb919eeb629054ee212e803e610c3f0ecf5303b0
Marc Davies <marcdaviesriot@gmail.com>
2020-01-02 21:46:24 -0800

    add client copy for farm stands and agrotourism page

--- farm-stands-and-agritourism.php
+++ farm-stands-and-agritourism.php
@@ -5,8 +5,61 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>
+        Farm stands and agritourism are a growing part of successful farm business diversification. The Oregon Farm Bureau publishes the Oregon's Bounty Farm Stand Guide each year (click <a href="https://oregonfb.org/oregonsbounty/">HERE</a> for more information). The guide provide easy access to MCFB member farm stands and beyond. In addition, MCFB promotes local farm stands every year on this website and its social media platforms. 
+    </p>
+    <p>
+        If you would like your local farm stand or agritourism opportunity listed below, please <a href="contact.php">contact us</a>.
+    </p>
+    <h6>
+        Bella Organic Pumpkin Patch & Winery
+    </h6>
+        <ul>
+            <li>Offers: U-pick strawberries, blueberries & blackberries; organic produce: heirloom tomatoes, green beans, garlic, carrots, corn, asparagus, zucchini, cucumbers, onions, leeks, cauliflower, peppers, lettuce, swiss chard, squash & berry wine, CSA</li>
+            <li>Events: Farm tours, pumpkin patch, hay rides, corn maze, haunted corn maze, Dogathon 5K Run, food, beer, wine tasting events with live music</li>
+            <li>Open: June-Nov, daily, 9:00-6:00</li>
+            <li>Website: <a href="http://www.bellaorganic.com">bellaorganic.com</a></li>
+            <li>Address: 16205 NW Gillihan Rd, Portland</li>
+            <li>Phone Number: 503.621.9545</li>
+        </ul>
+    <h6>
+        Brian Trout Ranch
+    </h6>
+        <ul>
+            <li>Offers: Live rainbow trout for pond and lake stocking</li>
+            <li>Open: All year, call for information</li>
+            <li>Website: <a href="http://www.briantroutranch.com">briantroutranch.com</a></li>
+            <li>Address: 26230 SE Brian Ranch Rd, Sandy</li>
+            <li>Phone Number: 503.668.7861</li>
+        </ul>
+    <h6>
+        Burns Farm
+    </h6>
     <ul>
-        <li>TBD</li>
+        <li>Offers: U-pick, we-pick raspberries & marionberries; farm stand with seasonal fruits & vegetables, fall decor & pumpkin patch</li>
+        <li>Open: May thru November, daily, 9:00-6:00 (call for availibility)</li>
+        <li>Address: 2318 SE 302nd Ave, Troutdale</li>
+        <li>Phone Number: 503.667.4380</li>
+    </ul>
+    <h6>
+        Busue Farm
+    </h6>
+    <ul>
+        <li>Offers:</li>
+        <li>Open:</li>
+        <li>Website:</li>
+        <li>Address:</li>
+        <li>Phone Number:</li>   
+    </ul>
+    <h6>
+        Rossi Farm
+    </h6>
+    <ul>
+        <li>Offers:</li>
+        <li>Open:</li>
+        <li>Website:</li>
+        <li>Address:</li>
+        <li>Phone Number:</li>   
     </ul>
     <p class="text-center">
         <a href="connecting-farmers.php" class="btn btn-primary">&larr; Back</a>
