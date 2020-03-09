ee8361244a2549f1caa57d524b5804a92572b3de
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 14:44:53 -0700

    update copy as per customer's request

--- board-meetings.php
+++ board-meetings.php
@@ -6,24 +6,106 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p>
-        MCFB Board monthly meetings and the annual meeting are normally held as per the schedule below. Notices, locations, and changes to this schedule are provided through newsletters, mailers, and on the MCFB Facebook page. Voting and Supporting members are always welcome to attend any of these meetings. Associate members, members of the public, and partner representatives should <a href="contact.php">contact us</a> if you would like to attend a meeting.
+        MCFB Board meetings and annual meeting are normally held as per the schedule below. Notices, locations, and changes to this schedule are provided through mailers and on the MCFB Facebook page. Voting and Supporting members are always welcome to attend any of these meetings. Associate members, members of the public, and partner representatives should <a href="contact.php">Contact Us</a> if you would like to attend a meeting.
     </p>
-    <ul>
-        <li>January, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
-        <li>February, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
-        <li>March, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
-        <li>April, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
-        <li>May, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
-        <li>June: off for farming</li>
-        <li>July: off for farming</li>
-        <li>August: off for farming</li>
-        <li>September, 3rd Thursday at 7:00 PM: Monthly Board Meeting</li>
-        <li>October, 3rd Thursday at 7:00 PM:Monthly  Board Meeting</li>
-        <li>November, Day/Time/Place TBD: Annual Board Meeting</li>
-        <li>December, Day/Time/Place TBD: Annual End-of-Year Holiday Party</li>
-    </ul>
+    <div class="container">
+        <table class="table table-bordered text-center">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">Month</th>
+                    <th scope="col">
+                        Board Meeting<br>
+                        3rd Thursday @ 7:00PM<br>
+                        1101 NE Burnside, Gresham
+                    </th>
+                    <th scope="col">
+                        Annual Meeting<br>
+                        Location, date and time TBD
+                    </th>
+                    <th scope="col">
+                        Holiday Gathering<br>
+                        Location, date and time TBD
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">January</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">February</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">March</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">April</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">May</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">June</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">July</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">August</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">September</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">October</th>
+                    <td>X</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">November</th>
+                    <td></td>
+                    <td>X</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">December</th>
+                    <td></td>
+                    <td></td>
+                    <td>X</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <p class="text-center">
-        <a href="connecting-farmers.php" class="btn btn-primary">&larr; Back</a>
+        <a href="about.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
     </p>
     <?php include("statement.php"); ?>
