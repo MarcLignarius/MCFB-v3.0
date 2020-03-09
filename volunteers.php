68720129bb76e390f0f5b3a5171347148963e525
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 14:12:40 -0700

    update copy as per customer's request

--- volunteers.php
+++ volunteers.php
@@ -6,20 +6,23 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p class="text-center">
-        MCFB occassionally benefits from the time and expertise of volunteers.Volunteers could lead or help with activities such as website development and maintenance, social media management, and event hosting. If you would like to volunteer or have a volunteer idea, please contact us.
+        MCFB occasionally benefits from the time and expertise of volunteers. Volunteers lead or help with activities such as website development and maintenance, social media management, and event hosting. If you would like to volunteer or have a volunteer idea, please Contact Us.
     </p>
-    <p>
+    <p class="font-weight-bold">
         Current MCFB Volunteers:
     </p>
-    <ul>
-        <li>Marc Davies</li>
-    </ul>
+    <p class="font-weight-bold">
+        Marc Davies, Web Developer
+    </p>
     <p>
+        Marc is a web developer who has been working on MCFB’s website since the fall of 2019. Marc has a Bachelor of Arts in International Business from the University of Portsmouth and a Master’s degree in International Commerce from the KEDGE Business School. Marc has also studied computer programming.
+    </p>
+    <p class="font-weight-bold">
         Current Volunteer Opportunities:
     </p>
-    <ul>
-        <li>Social Media Volunteer. <a href="contact.php">Contact Us</a> if interested.</li>
-    </ul>
+    <p>
+        Social Media Volunteer. <a href="contact.php">Contact Us</a> if interested.
+    </p>
     <p class="text-center">
         <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
