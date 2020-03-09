2e149292049fa6988ff82e3ae96a6b1dc0d9a224
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 15:06:20 -0700

    update copy as per customer's request

--- farm-stands-and-agritourism.php
+++ farm-stands-and-agritourism.php
@@ -6,60 +6,60 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p>
-        Farm stands and agritourism are a growing part of successful farm business diversification. The Oregon Farm Bureau publishes the Oregon's Bounty Farm Stand Guide each year (click <a href="https://oregonfb.org/oregonsbounty/">HERE</a> for more information). The guide provide easy access to MCFB member farm stands and beyond. In addition, MCFB promotes local farm stands every year on this website and its social media platforms. 
+        Farm stands and agritourism are a growing part of many successful farm businesses. OFB publishes <a href="https://oregonfb.org/oregonsbounty/">Oregon's Bounty Farm Stand Guide</a> each year. The guide provides easy access to OFB member farm stands and beyond. In addition, MCFB promotes local farm stands every year on this website and social media to assist local farmers in getting the word out to our communities about their offerings.
     </p>
     <p>
-        If you would like your local farm stand or agritourism opportunity listed below, please <a href="contact.php">contact us</a>.
+    If you would like your local farm stand or agritourism opportunity listed below and publicized on the MCFB Facebook site, please <a href="contact.php">Contact Us</a>.
     </p>
-    <h6>
+    <p class="font-weight-bold">
         Bella Organic Pumpkin Patch & Winery
-    </h6>
-        <ul>
-            <li>Offers: U-pick strawberries, blueberries & blackberries; organic produce: heirloom tomatoes, green beans, garlic, carrots, corn, asparagus, zucchini, cucumbers, onions, leeks, cauliflower, peppers, lettuce, swiss chard, squash & berry wine, CSA</li>
-            <li>Events: Farm tours, pumpkin patch, hay rides, corn maze, haunted corn maze, Dogathon 5K Run, food, beer, wine tasting events with live music</li>
-            <li>Open: June-Nov, daily, 9:00-6:00</li>
-            <li>Website: <a href="http://www.bellaorganic.com">bellaorganic.com</a></li>
-            <li>Address: 16205 NW Gillihan Rd, Portland</li>
-            <li>Phone Number: 503.621.9545</li>
-        </ul>
-    <h6>
+    </p>
+    <ul>
+        <li>Offers: U-pick strawberries, blueberries & blackberries; organic produce: heirloom tomatoes, green beans, garlic, carrots, corn, asparagus, zucchini, cucumbers, onions, leeks, cauliflower, peppers, lettuce, swiss chard, squash & berry wine, CSA</li>
+        <li>Events: Farm tours, pumpkin patch, hay rides, corn maze, haunted corn maze, Dogathon 5K Run, food, beer, wine tasting events with live music</li>
+        <li>Open: June-Nov, daily, 9:00-6:00</li>
+        <li>Website: <a href="http://www.bellaorganic.com">bellaorganic.com</a></li>
+        <li>Address: 16205 NW Gillihan Rd, Portland</li>
+        <li>Phone Number: 503.621.9545</li>
+    </ul>
+    <p class="font-weight-bold">
         Brian Trout Ranch
-    </h6>
-        <ul>
-            <li>Offers: Live rainbow trout for pond and lake stocking</li>
-            <li>Open: All year, call for information</li>
-            <li>Website: <a href="http://www.briantroutranch.com">briantroutranch.com</a></li>
-            <li>Address: 26230 SE Brian Ranch Rd, Sandy</li>
-            <li>Phone Number: 503.668.7861</li>
-        </ul>
-    <h6>
+    </p>
+    <ul>
+        <li>Offers: Live rainbow trout for pond and lake stocking</li>
+        <li>Open: All year, call for information</li>
+        <li>Website: <a href="http://www.briantroutranch.com">briantroutranch.com</a></li>
+        <li>Address: 26230 SE Brian Ranch Rd, Sandy</li>
+        <li>Phone Number: 503.668.7861</li>
+    </ul>
+    <p class="font-weight-bold">
         Burns Farm
-    </h6>
+    </p>
     <ul>
         <li>Offers: U-pick, we-pick raspberries & marionberries; farm stand with seasonal fruits & vegetables, fall decor & pumpkin patch</li>
         <li>Open: May thru November, daily, 9:00-6:00 (call for availibility)</li>
         <li>Address: 2318 SE 302nd Ave, Troutdale</li>
         <li>Phone Number: 503.667.4380</li>
     </ul>
-    <h6>
-        Busue Farm
-    </h6>
+    <p class="font-weight-bold">
+        Bushue’s Family Farm
+    </p>
     <ul>
-        <li>Offers:</li>
-        <li>Open:</li>
-        <li>Website:</li>
-        <li>Address:</li>
-        <li>Phone Number:</li>   
+        <li>Offers: From May through June, greenhouses are filled with all the premium annuals, bedding plants, flowering baskets, patio containers and vegetable starts you need to get your garden going. U-Pick starting with strawberries in June, moving into raspberries, green beans, tomatoes and sweet and hot peppers through the summer and into the fall. Fall season brings pumpkins, hayrides, hayslide adventure park, Lil’ Pumpkin’s Playground, pig train rides, pumpkin bowling alley, dirt babies, corn trail, and farm animals.</li>
+        <li>Open: Flower Season (May to July): Wednesday to Saturday, 10am to 4pm. Sunday, 11am to 4pm. U-Pick Season (June to October): Depending on crop availability. Pumpkin Season (October): Tuesday to Sunday, 10am to 5pm</li>
+        <li>Website: <a href="https://bushuefarming.com/">bushuefarming.com</a></li>
+        <li>Address: 9880 SE Revenue Rd, Boring</li>
+        <li>Phone Number: 503-307-6074</li>
     </ul>
-    <h6>
-        Rossi Farm
-    </h6>
+    <p class="font-weight-bold">
+        Rossi Farms
+    </p>
     <ul>
-        <li>Offers:</li>
-        <li>Open:</li>
-        <li>Website:</li>
-        <li>Address:</li>
-        <li>Phone Number:</li>   
+        <li>Offers: Farm is available to rent for festivals, corporate, and private events whose proceeds help benefit the local community and Parkrose High School athletic programs. Farm stand closed in 2015.</li>
+        <li>Open: Year round</li>
+        <li>Website: <a href="http://www.rossifarms.com/">rossifarms.com</a></li>
+        <li>Address: 3839 NE 122nd Ave, Portland</li>
+        <li>Phone Number: 503-253-5571</li>
     </ul>
     <p class="text-center">
         <a href="assisting-farmers.php" class="btn btn-primary">&larr; Back</a>
