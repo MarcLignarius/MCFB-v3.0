cf07823ac00f3153d2a9bcd7f887df95e00e9a8d
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 14:09:07 -0700

    update copy as per customer's request

--- contractors.php
+++ contractors.php
@@ -8,19 +8,26 @@
     <p class="text-center">
         MCFB does not have employees. Currently, MCFB contracts for services related to the executive director/secretary and treasurer.
     </p>
-    <p>
+    <p class="font-weight-bold">
         Current MCFB Contractors:
     </p>
-    <ul>
-        <li>Jay Udelhoven, Executive Director/Secretary</li>
-        <li>Deniece Tucker, Treasurer</li>
-    </ul>
+    <p class="font-weight-bold">
+        Jay Udelhoven, Executive Director
+    </p>
     <p>
+        Jay grew up in the Midwest, running around his grandparent's and uncle's dairy farms. Throughout his 30-year natural resource career, he has worked on numerous farm-related projects including dairy production in Africa, fish production in Panama, Mexico and Washington State, cattle ranching in Florida, and small-scale nursery/berry/mixed vegetable production in Oregon. Jay has served in senior level positions for several organizations over the past 20 years and has served as the MCFB Executive Director since July 2019. He holds a Bachelor of Science in Natural Resources from the University of Wisconsin-Madison and a Master of Environmental Policy from the University of Denver, Colorado.
+    </p>
+    <p class="font-weight-bold">
+        Deniece Tucker, Treasurer
+    </p>
+    <p>
+        Deniece was born and raised on her family’s farm located east of Troutdale near the Sandy River. The property was homesteaded in 1853 and has been farmed since 1952. Deniece and her family historically raised cattle and grew raspberries but converted to growing nursery stock in 2001 due to market changes. A member of the Multnomah County Farm Bureau since 1999, Deniece joined the Board of Directors in 2000 and has served as the Board President (2007-2008) and Vice-President (2009-2018). Deniece is currently serving as the Board Treasurer. Deniece holds an Associate Degree in Accounting, is an Oregon licensed tax consultant, and is a federally licensed tax agent.
+    </p>
+    <p class="font-weight-bold">
         Current Contract Opportunities:
     </p>
-    <ul>
-        <li>None at this time. <a href="contact.php">Contact Us</a> if you have a suggestion.</li>
-    </ul>
+    <p>
+    None at this time. <a href="contact.php">Contact Us</a> if you have a suggestion.
     </p>
     <p class="text-center">
         <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
