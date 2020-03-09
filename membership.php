c8fce27731acfa02d4e39cb68b543dfd40986e7d
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 13:48:11 -0700

    update copy as per customer's request

--- membership.php
+++ membership.php
@@ -9,64 +9,50 @@
     <p>
         There are three levels of membership:
     </p>
-    <div class="row">
-        <div class="col-lg-4 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">Voting members ($125/year)</h2>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">Supporting members ($125/year)</h2>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">Associate members ($20/year)</h2>
-                </div>
-            </div>
-        </div>
-    </div>
+    <ol class="mb-4">
+        <li>Voting members</li>
+        <li>Supporting members</li>
+        <li>Associate members</li>
+    </ol>
+    <h5>Voting Members</h5>
     <p>
-        Farmers and ranchers with $2,500 or more/year in gross farming/ranching income qualify as voting members. Voting members may:
+        Farmers and ranchers with $2,500 or more per year in gross farming or ranching income
+        qualify as voting members. Voting membership costs $125 per year.
     </p>
-    <ul>
+    <p>Voting members may (but are not required to):</p>
+    <ul class="mb-4">
         <li>Attend all county board meetings, annual meetings, events, and outings</li>
-        <li>Vote annually at the annual county meeting for board directors and officers</li>
+        <li>Vote annually at the annual county meeting for Board Directors and officers</li>
         <li>Attend the annual state and national conventions</li>
         <li>Run for county and state boards and officer positions</li>
         <li>Represent the county as a voting delegate at the annual state convention</li>
-        <li>Receive all of the benefits from the state farm bureau</li>
+        <li>Receive all <a href="https://oregonfb.org/member-discounts-2/">OFB benefits</a></li>
     </ul>
+    <h5>Supporting Members</h5>
     <p>
-        Click <a href="benefits.php" target="blank">HERE</a> to learn more about benefits.
+        Farmers and ranchers with less than $2,500 per year in gross farming or ranching
+        income and non-farmers qualify as supporting members. Supporting membership costs $125
+        per year.
     </p>
-    <p>
-        Farmers and ranchers with less than $2,500/year in gross farming/ranching income and non-farmers qualify as supporting members. Supporting members may:
-    </p>
-    <ul>
+    <p>Supporting members may (but are not required to):</p>
+    <ul class="mb-4">
         <li>Attend all county board meetings, annual meetings, events, and outings</li>
         <li>Attend the annual state and national conventions</li>
-        <li>Receive all of the benefits from the state farm bureau</li>
+        <li>Receive all <a href="https://oregonfb.org/member-discounts-2/">OFB benefits</a></li>
     </ul>
     <p>
-        Click <a href="benefits.php" target="blank">HERE</a> to learn more about benefits.
+        Voting and supporting membership can be attained by completing a hard copy membership packet, by completing the
+        <a href="https://oregonfb.org/join/" target="blank">OFB's online application</a> or by signing up for OFB
+        through <a href="https://www.countryfinancial.com/">Country Financial</a>. For more information, <a href="contact.php">Contact Us</a>.
     </p>
+    <h5>Associate Members</h5>
     <p>
-        Membership can be attained by completing a hard copy membership packet, by completing the OFB's
-        <a href="https://oregonfb.org/join/" target="blank">online application</a> or by signing up for OFB
-        through Country Financial. For more information, <a href="contact.php">contact MCFB</a>.
-    </p>
-    <p>
-        Through a special arrangement with Country Financial, all Country Financial customers are automatically associate members. Only Country Financial customers can be associate members. Associate members support the MCFB and OFB financially with annual dues and may attend meetings, events, and outings with approval from the MCFB Board President or Executive Director.
+        Through a <a href="https://oregonfb.org/about/ofb-country-financial/">special arrangement</a> with <a href="https://www.countryfinancial.com/">Country Financial</a>, all non-farmer/non-rancher
+        Country Financial customers are automatically OFB associate members. Associate
+        members living in Multnomah County become MCFB associate members. Only Country
+        Financial customers can be associate members. Associate members support the MCFB
+        and OFB financially with annual dues and may attend meetings, events, and outings with
+        approval from the MCFB Board President or Executive Director.
     </p>
     <p class="text-center">
         <a href="our-people.php" class="btn btn-primary">&larr; Back</a>
