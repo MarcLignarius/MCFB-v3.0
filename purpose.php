3fc095adef58ab54b148a2dcb825519f90d8a167
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 13:32:51 -0700

    update copy as per customer's request

--- purpose.php
+++ purpose.php
@@ -6,19 +6,26 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p class="text-center">
-        MCFB’s Purpose is represented by our vision, mission, and values. These represent what we stand for and work towards. The vision, mission, and values guide all of our actions. Opportunities put before the board for decisions will be evaluated based on well the intended outcomes support our vision, mission, and values. Our vision, mission, and values are below.
+        MCFB’s purpose is represented by our vision, mission, and values. These represent what we
+        stand for and work towards. The vision, mission, and values guide all of our actions. The Board
+        of Directors will evaluate opportunities and actions based on how well the intended outcomes
+        support our vision, mission, and values.
+        Our vision, mission, and values are below.
     </p>
     <p class="text-center">
-        MCFB’s Vision: Farmers experience high quality livelihoods by growing world class products that ultimately support vibrant, healthy, and sustainable farms and communities.
+        MCFB’s Vision: Farmers experience high quality livelihoods by growing world class products
+        that support vibrant, healthy, and sustainable farms and communities.
     </p>
     <p class="text-center">
         MCFB's Mission: We help local farmers be successful.
     </p>
     <p class="text-center">
-        MCFB's Values: MCFB believes that healthier, happier, and more prosperous farming communities can be supported through greater human diversity, equity, and inclusion within our local farms and our organization.
+        MCFB's Values: MCFB believes that healthier, happier, and more prosperous farming
+        communities are supported through greater human diversity, equity, and inclusion within our
+        local farms and our organization.
     </p>
     <p class="text-center">
-        MCFB's Approach: Click <a href="purpose-our-approach.php"> HERE </a> to learn more about the actions we use to implement our Purpose.
+        MCFB's Approach: <a href="purpose-our-approach.php"> Our Approach </a> explains the actions we use to implement our purpose.
     </p>
     <p class="text-center">
         <a href="about.php" class="btn btn-primary">&larr; Back</a>
