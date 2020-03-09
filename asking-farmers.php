41e732637f9bb9312f9ef656df60c90bb019e3da
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 14:49:17 -0700

    update copy as per customer's request

--- asking-farmers.php
+++ asking-farmers.php
@@ -9,16 +9,17 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p class="text-center">
-        People have questions about farming. Whether members of the general public, students, industry representatives, government employees, or elected officials, when farm-related questions come up, people tend to turn to the usual information sources. These information sources often consist of friends, colleagues, books, the Internet, or academic experts. Rarely, though, do people have ready access to real farmers. Real farmers have unique experiences, knowledge, and understanding of land and farming techniques and issues. Farmers, local communities, and our society in general will benefit if people who are seeking to understand farming better have the ability to ask farmers directliy. Given this, MCFB wants to connect people with questions about farming with real farmers. To accomplish this, we will establish the “Ask a Farmer” program.
+        People have questions about farming. Whether members of the general public, students, industry representatives, government employees, or elected officials, when farm-related questions come up, people tend to turn to the usual information sources. These information sources often consist of friends, colleagues, books, the Internet, or academic experts. Rarely, though, do people have ready access to real farmers. Real farmers have unique experiences, knowledge, and understanding of land and farming techniques and issues. Farmers, local communities, and our society in general will benefit if people who are seeking to understand farming better have the ability to ask farmers directly. Given this, MCFB wants to connect people with questions about farming with real farmers. To accomplish this, we have established the “Ask a Farmer” program.
     </p>
-    <p>
+    <p class="font-weight-bold">
         Recent questions posed to our farmers include:
     </p>
     <ul>
-        <li>How much does local farmland lease for per year?</li>
+        <li>What are typical local farmland lease rates?</li>
         <li>What does a farm business plan look like?</li>
         <li>What is it like to have a solar field within a farm?</li>
         <li>How much water is required annually for a single horse?</li>
+        <li>Is there a lease premium for farmland that has been in the Conservation Reserve Program?</li>
     </ul>
     <p>
         If you have a question that you would like to pose to a local farmer, <a href="contact.php">Contact Us</a>.
