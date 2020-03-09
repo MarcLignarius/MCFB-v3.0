c3f5e65e7811ab6044f3929aa7e4731f1a988be4
Marc Davies <marcdaviesriot@gmail.com>
2020-03-09 13:33:55 -0700

    update copy as per customer's request

--- farming.php
+++ farming.php
@@ -6,11 +6,16 @@
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
     <p class="text-center">
-    Multnomah County encompasses over 300,000 acres and includes both highly urbanized and less developed rural communities. Within the county, there are over 650 farms made up of more than 25,000 acres. These farms are both big and small, urban and rural, organic and conventional, and food and non-food producing. Multnomah County farm commodity sales total over $74 million annually and are thus a critical part of the local economy. Farmers from all sectors are welcomed, encouraged to join and particiate, and included in MCFB.	
+        Multnomah County encompasses over 300,000 acres and includes both highly urbanized and
+        less developed rural communities. Within the county, there are over 650 farms made up of
+        more than 25,000 acres. These farms are both big and small, urban and rural, organic and
+        conventional, and food and non-food producing. Multnomah County farm commodity sales total
+        over $74 million annually and are thus a critical part of the local economy. Farmers from all
+        sectors are welcome and encouraged to join and participate in MCFB.
     </p>
     <p class="text-center">
-        <a href="about.php" class="btn btn-primary">&larr; Back</a>   
-        <a href="index.php" class="btn btn-primary">Home</a> 
+        <a href="about.php" class="btn btn-primary">&larr; Back</a>
+        <a href="index.php" class="btn btn-primary">Home</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
