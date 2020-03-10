15644e907200820e39577097477f6b52e4cbce20
Marc Davies <marcdaviesriot@gmail.com>
2020-03-10 10:28:47 -0700

    update copy as per customer's request

--- organizational-support.php
+++ organizational-support.php
@@ -5,9 +5,14 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>As a means of representing farmers, MCFB provides financial support to two types of organizations: 1) Organizations that serve the greater good of the community; and 2) Organizations that specifically advocate on behalf of farmers. Organizations typically receiving MCFB support include:</p>
     <ul>
-        <li>OFB PAC Trap Shoot</li>
-        <li>Oregonians for Food & Shelter</li>
+        <li><a href="https://www.oan.org/page/chapters">Mt. Hood Chapter of the Oregon Association of Nurseries</a></li>
+        <li><a href="https://www.ofsonline.org/">Oregonians for Food and Shelter</a></li>
+        <li><a href="https://oregonfb.org/tag/ofb-pac/">Oregon Farm Bureau Political Action Committee</a></li>
+        <li><a href="https://oregonfb.org/programs/oregon-agriculture-educational-foundation-oaef/">Oregon Farm Bureau Voter Education Fund</a></li>
+        <li><a href="https://www.rmhc.org/">Ronald McDonald House</a></li>
+        <li><a href="https://www.wrlegal.org/">Western Resources Legal Center</a></li>
     </ul>
     <p class="text-center">
         <a href="representing-farmers.php" class="btn btn-primary">&larr; Back</a>
