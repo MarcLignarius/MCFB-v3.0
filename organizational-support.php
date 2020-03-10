ccf388a21f88b35091fcb69e0ade8ec8fb21bbb6
Marc Davies <marcdaviesriot@gmail.com>
2020-03-10 10:30:49 -0700

    update copy as per customer's request

--- organizational-support.php
+++ organizational-support.php
@@ -5,7 +5,12 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
-    <p>As a means of representing farmers, MCFB provides financial support to two types of organizations: 1) Organizations that serve the greater good of the community; and 2) Organizations that specifically advocate on behalf of farmers. Organizations typically receiving MCFB support include:</p>
+    <p>As a means of representing farmers, MCFB provides financial support to two types of organizations:</p>
+    <ul>
+        <li>Organizations that serve the greater good of the community</li>
+        <li>Organizations that specifically advocate on behalf of farmers.</li>
+    </ul>
+    <p>Organizations typically receiving MCFB support include:</p>
     <ul>
         <li><a href="https://www.oan.org/page/chapters">Mt. Hood Chapter of the Oregon Association of Nurseries</a></li>
         <li><a href="https://www.ofsonline.org/">Oregonians for Food and Shelter</a></li>
