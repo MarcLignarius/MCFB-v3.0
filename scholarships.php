6125fd6981c1479f13c5c1af7bfb604aef91e7a2
Marc Davies <marcdaviesriot@gmail.com>
2020-03-10 11:04:40 -0700

    update copy as per customer's request

--- scholarships.php
+++ scholarships.php
@@ -5,10 +5,18 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>MCFB contributes to various scholarships related to agricultural education throughout each year. Some of these contributions are recurring events while others are one-time occurrences. Scholarship opportunities are noticed through our website, Facebook page, mailers, and email as they become available.</p>
+    <p>Scholarships that MCFB members may qualify for include, but are not limited to:</p>
     <ul>
-        <li>OFB memorial scholarship program</li>
-        <li>Oregon Agricultural Education Foundation (OAEF) Summer Agriculture Institute (SAI)</li>
+        <li>Pacific Building Systems National Ag Day Scholarships (students from OFB member families only)</li>
+        <li>OFB Memorial Scholarships (OFB voting member children or grandchildren only)</li>
+        <li>Associate Member Scholarship (OFB associate members only)</li>
     </ul>
+    <p>For more information on the above scholarships, see: <a href="https://oregonfb.org/scholarships/">https://oregonfb.org/scholarships/</a>.</p>
+    <p>Other scholarships that MCFB has contributed to include:</p>
+    <ul>
+        <li><a href="https://agsci.oregonstate.edu/eoarc-union/summer-ag-institute">Oregon Agricultural Education Foundation Summer Agriculture Institute</a> (Multnomah County and Clackamas County teachers only)</li>
+    </ul>    
     <p class="text-center">
         <a href="sharing-knowledge-with-farmers.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
