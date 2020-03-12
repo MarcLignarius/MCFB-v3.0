09d4f8978de4a66cea7d819f22fdf3acb606a50a
Marc Davies <marcdaviesriot@gmail.com>
2020-03-11 20:14:35 -0700

    update copy as per customer's request

--- conferences.php
+++ conferences.php
@@ -5,9 +5,10 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>Each year MCFB provides a limited number of sponsorships to enable MCFB voting and supporting members to attend agriculture-related conferences. Voting and supporting members are encouraged to express interest in conference attendance early in the year if possible. Conferences that are attended each year by MCFB voting and supporting members are listed below:</p>
     <ul>
-        <li>OFB Annual Conference</li>
-        <li>AFBF Annual Conference</li>
+        <li><a href="https://oregonfb.org/convention/">OFB Annual Conference</a> (normally in December of each year)</li>
+        <li><a href="https://www.fb.org/events/afbf-annual-convention/">AFBF Annual Conference</a> (normally in January of each year)</li>
     </ul>
     <p class="text-center">
         <a href="sharing-knowledge-with-farmers.php" class="btn btn-primary">&larr; Back</a>
