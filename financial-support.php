05765836c89aa099a98d9dd42a985132d90c1953
Marc Davies <marcdaviesriot@gmail.com>
2020-03-11 20:30:56 -0700

    update copy as per customer's request

--- financial-support.php
+++ financial-support.php
@@ -5,9 +5,11 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>MCFB provides financial support to organizations that teach and train future and current farmers. A list of currently supported organizations is below. MCFB entertains requests for organizational support on an on-going basis. If you would like to request support, please <a href="contact.php">Contact Us</a>.</p>
     <ul>
-        <li>Zenger Farm</li>
-        <li>Headwaters Incubator Program</li>
+        <li><a href="https://oregonfb.org/programs/oregon-agriculture-educational-foundation-oaef/">Oregon Agricultural Education Foundation</a></li>
+        <li><a href="http://realoregon.net/">REAL Oregon</a></li>
+        <li><a href="http://sandyffa.theaet.com/aethome.aspx?ID=30094">Sandy FFA</a></li>
     </ul>
     <p class="text-center">
         <a href="sharing-knowledge-with-farmers.php" class="btn btn-primary">&larr; Back</a>
