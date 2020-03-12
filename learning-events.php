7fd28871348b80f5ff8fb6bef4cb43fcd4bb0b19
Marc Davies <marcdaviesriot@gmail.com>
2020-03-11 20:18:07 -0700

    update copy as per customer's request

--- learning-events.php
+++ learning-events.php
@@ -5,10 +5,11 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>MCFB sponsors events, groups, and individuals to increase local community members’ exposure to agricultural issues and educational opportunities. Learning events that are typically sponsored include those listed below. MCFB is interested to learn of new agriculture-related learning events that local farmers and community members would like to attend. </p>
     <ul>
-        <li>Oregon Ag Fest</li>
-        <li>Sandy FFA National Skit Competition</li>
-        <li>4H & FFA Sandy Area Buyer's Pool/Auction at Clackamas County Fair</li>
+        <li><a href="http://sandyffa.theaet.com/aethome.aspx?ID=30094">Sandy Future Farmers of America</a> participation at <a href="https://www.ffa.org/participate/cde-lde/">National Events</a></li>
+        <li>Sandy Areas Buyers Pool, which supports 4-H and FFA members participation in the Clackamas County Fair Jr. Livestock Auction</li>
+        <li><a href="http://oragfest.com/">Oregon Ag Fest</a></li>
     </ul>
     <p class="text-center">
         <a href="sharing-knowledge-with-farmers.php" class="btn btn-primary">&larr; Back</a>
