39964a137339b7e56ea8673e5f9da1d343d03f1a
Marc Davies <marcdaviesriot@gmail.com>
2020-03-11 20:21:58 -0700

    update copy as per customer's request

--- workshops.php
+++ workshops.php
@@ -5,23 +5,14 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+    <p>MCFB sponsors and hosts workshops to help local farmers achieve necessary certifications and licenses and remain compliant with laws and regulations. We are developing an annual “curriculum” that farmers can depend on every year. The curriculum will include two workshop topics that are the same from year to year and two workshop topics that change from year to year. Our current workshop training schedule is below. Workshops will be announced through email, mailers, and the MCFB Facebook site.</p>
     <ul>
-        <li>Pesticide Applicators Licensing</li>
-        <li>Worker Protection Standard for Agricultural Workers/Farm Safety/First Aid & CPR</li>
-        <li>Agrivoltaics (crops & solar)</li>
-        <li>Irrigation Management</li>
-        <li>Drones in Agriculture</li>
-        <li>Hemp & Marijuana Production & Processing</li>
-        <li>Wildlife Management</li>
-        <li>Agricultural Water Quality Rules - ODA</li>
-        <li>Conservation Practices - EMSWCD/NRCS</li>
-        <li>Agricultural Easements - EMSWCD/NRCS</li>
-        <li>Farm Succession Planning - EMSWCD</li>
-        <li>Agricultural Marketing - FSA AMS</li>
-        <li>Crop Diversification - FSA RMA</li>
-        <li>Farm Loan Programs - FSA</li>
-        <li>Farm Insurance Options - FSA/Country Financial</li>
+        <li><strong>January/February:</strong> Worker Protection Standards & Pesticides for Owners/Managers</li>
+        <li><strong>January/February:</strong> Worker Protection Standards & Pesticides for Workers and Handlers</li>
+        <li><strong>January/February:</strong> Rotating training topic</li>
+        <li><strong>January/February:</strong> Rotating training topic</li>
     </ul>
+    <p>If you are a local farmer and would like to request a training topic, please <a href="contact.php">Contact Us</a>.</p>
     <p class="text-center">
         <a href="sharing-knowledge-with-farmers.php" class="btn btn-primary">&larr; Back</a>
         <a href="index.php" class="btn btn-primary">Home</a>
