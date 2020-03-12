ddbc07e8975010a0159999895e1be6f75fc877b9
Marc Davies <marcdaviesriot@gmail.com>
2020-03-11 20:37:17 -0700

    update copy as per customer's request

--- benefits.php
+++ benefits.php
@@ -5,10 +5,22 @@
 
 <div class="container-fluid">
     <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
-    <p class="text-center">
-        Our Approach: <a href="benefits-our-approach.php" class="btn btn-link">Link to page &rarr;</a>
+    <p class="font-weight-bold">What benefits do MCFB member receive?</p>
+    <p>
+    In addition to our program benefits explained in <a href="benefits-our-approach.php">Our Approach</a>, MCFB members also receive all OFB statewide and AFBF nationwide benefits, which include access to, inclusion in, or influence over:
     </p>
-
+    <ul>
+        <li>Oregon’s Bounty and OFB Buyer’s Guide</li>
+        <li>Scholarships, Young Farmers & Ranchers, and Women’s Advisory Council</li>
+        <li>Ag education and health & safety</li>
+        <li>Foundation for Education</li>
+        <li>Century Farm & Ranch</li>
+        <li>Summer Ag Institute</li>
+        <li>Farm Employer Education & Legal Defense Service</li>
+        <li>Resource Education & Agriculture Leadership</li>
+        <li>Oregon Agricultural Legal Foundation</li>
+    </ul>
+    <p>MCFB members also receive discounts on nearly 30 resources and services, such as new farm vehicles and equipment, solar panels, health insurance, and theft recovery. These benefits are constantly improving so members should check the <a href="https://oregonfb.org/member-discounts-2/">OFB website</a> to verify current offerings.</p>
     <p class="text-center">
         <a href="index.php" class="btn btn-primary">&larr; Back</a>
     </p>
@@ -16,135 +28,3 @@
 </div>
 
 <?php include("footer.php"); ?>
-
-<!-- <ul>
-    <li>
-        Training, education, outreach, and certification on programs and topics necessary
-        for farm production success.
-        <a href="sharing-knowledge-with-farmers.php" class="btn btn-link">Learn More &rarr;</a></li>
-    <li>
-        Advocacy, representation, and a voice on local government and statewide issues and
-        policies affecting farmers.
-        <a href="representing-farmers.php" class="btn btn-link">Learn More &rarr;</a></li>
-    <li>
-        Networking, meetings, and social gatherings that offer networking with local farmers
-        and supporters who exchange information and provide assistance to one another.
-        <a href="connecting-farmers.php" class="btn btn-link">Learn More &rarr;</a>
-    </li>
-    <li>
-        On-farm assistance and community projects to address issues affecting farm success.
-        <a href="on-farm-and-community-projects-index.php" class="btn btn-link">Learn More &rarr;</a>
-    </li>
-</ul> -->
-
-<!-- 
-    <h3>Are there wider benefits?</h3>
-    <p>
-        MCFB members also receive all OFB statewide and American Farm Bureau Federation nationwide
-        benefits, which include access to, inclusion in, or influence over:
-    </p>
-    <ul>
-        <li>
-            Oregon’s Bounty and OFB Buyer’s Guide
-        <li>
-            Scholarships, Young Farmers & Ranchers, and Women’s Advisory Council
-        <li>
-            Agricultural education and health & safety
-        </li>
-        <li>
-            Foundation for Education
-        </li>
-        <li>
-            Century Farm & Ranch
-        </li>
-        <li>
-            Summer Ag Institute
-        </li>
-        <li>
-            Farm Employer Education & Legal Defense Service
-        </li>
-        <li>
-            Resource Education & Agriculture Leadership
-        </li>
-        <li>
-            Oregon Agricultural Legal Foundation
-        </li>
-    </ul>
-    <h3>Additional Benefits:</h3>
-    <p>
-        In addition to the above benefits, MCFB members receive discounts on nearly 30
-        resources and services, such as new farm vehicles and equipment, solar panels,
-        health insurance, and theft recovery. These benefits are constantly improving so members
-        should check the OFB website to verify current offerings.
-        <a href="https://oregonfb.org/member-discounts-2/" target="blank" class="btn btn-link">Learn More &rarr;</a>
-    </p>
-    <h3>Why should you be involved?</h3>
-    <p>
-        At the local level, MCFB members receive:
-    </p>
-    <div class="row">
-        <div class="col-xl-3 col-md-6 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">Training</h2>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                    </p>
-                </div>
-                <div>
-                    <a href="benefits-training-index.php" class="btn btn-link">Learn More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">Advocacy</h2>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                    </p>
-                </div>
-                <div>
-                    <a href="benefits-advocacy-index.php" class="btn btn-link">Learn More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">Networking</h2>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                    </p>
-                </div>
-                <div>
-                    <a href="benefits-networking-index.php" class="btn btn-link">Learn More &rarr;</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-3">
-            <div class="card h-100 text-center">
-                <div class="image-text-container">
-                    <img class="card-image" src="../images/placeholder.png" alt="">
-                    <h2 class="card-title">On-farm & Community Projects</h2>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, asperiores unde.
-                    </p>
-                </div>
-                <div>
-                    <a href="benefits-on-farm-and-community-projects-index.php" class="btn btn-link">Learn More &rarr;</a>
-                </div>
-            </div>
-        </div>
-    </div> -->
