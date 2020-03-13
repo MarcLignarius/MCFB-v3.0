055591d51ec0cb395244e3f587b6f33158cfac74
Marc Davies <marcdaviesriot@gmail.com>
2020-03-12 22:25:10 -0700

    cleanup contact page

--- contact.php
+++ contact.php
@@ -3,56 +3,50 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
-<div class="container-fluid">
+<div class="container">
   <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
-  <div class="row">
-    <div class="col-md-8">
-      <h3>Send us a Message</h3>
-      <form name="sentMessage" id="contactForm" novalidate>
-        <div class="control-group form-group">
-          <div class="controls">
-            <label>Full Name:</label>
-            <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
-            <p class="help-block"></p>
-          </div>
-        </div>
-        <div class="control-group form-group">
-          <div class="controls">
-            <label>Phone Number:</label>
-            <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
-          </div>
-        </div>
-        <div class="control-group form-group">
-          <div class="controls">
-            <label>Email Address:</label>
-            <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
-          </div>
-        </div>
-        <div class="control-group form-group">
-          <div class="controls">
-            <label>Message:</label>
-            <textarea rows="5" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
-          </div>
-        </div>
-        <div id="success"></div>
-        <!-- For success/fail messages -->
-        <p>
-          <button type="submit" class="btn btn-primary" id="sendMessageButton">Send Message</button>
-        </p>
-      </form>
+  <h3 class="mb-3">Contact Details:</h3>
+  <p>
+    <i class="fa fa-phone"></i>
+    <a href="tel:206-595-5078" class="ml-1" style="color: black; text-decoration: none">206-595-5078</a>
+  </p>
+  <p>
+    <i class="fa fa-envelope"></i>
+    <a href="mailto:multnomahCFB@gmail.com" class="ml-1" style="color: black; text-decoration: none">multnomahCFB@gmail.com</a>
+  </p>
+  <h3 class="mt-5">Send us a Message</h3>
+  <form name="sentMessage" id="contactForm" novalidate>
+    <div class="control-group form-group">
+      <div class="controls">
+        <label>Full Name:</label>
+        <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
+        <p class="help-block"></p>
+      </div>
     </div>
-    <div class="col-md-4">
-      <h3>Contact Details:</h3>
-      <p>
-        <i class="fa fa-phone"></i>
-        <a href="tel:206-595-5078" class="ml-1" style="color: black; text-decoration: none">206-595-5078</a>
-      </p>
-      <p>
-        <i class="fa fa-envelope"></i>
-        <a href="mailto:multnomahCFB@gmail.com" class="ml-1" style="color: black; text-decoration: none">multnomahCFB@gmail.com</a>
-      </p>
+    <div class="control-group form-group">
+      <div class="controls">
+        <label>Phone Number:</label>
+        <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
+      </div>
     </div>
-  </div>
+    <div class="control-group form-group">
+      <div class="controls">
+        <label>Email Address:</label>
+        <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+      </div>
+    </div>
+    <div class="control-group form-group">
+      <div class="controls">
+        <label>Message:</label>
+        <textarea rows="5" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+      </div>
+    </div>
+    <div id="success"></div>
+    <!-- For success/fail messages -->
+    <p>
+      <button type="submit" class="btn btn-primary" id="sendMessageButton">Send Message</button>
+    </p>
+  </form>
   <?php include("statement.php"); ?>
   <p class="text-center">
     <a href="index.php" class="btn btn-primary">Home</a>
