d3070fbfca2573b79619b82e732914ff6f00348d
Marc Davies <marcdaviesriot@gmail.com>
2020-03-13 13:52:37 -0700

    code cleanup

--- benefits.php
+++ benefits.php
@@ -3,28 +3,36 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="mb-4"><?php echo $page_title; ?></h1>
+        <p class="font-weight-bold">What benefits do MCFB member receive?</p>
+        <p>
+        In addition to our program benefits explained in <a href="benefits-our-approach.php">Our Approach</a>, MCFB members also receive all 
+        OFB statewide and AFBF nationwide benefits, which include access to, inclusion in, or influence over:
+        </p>
+        <ul>
+            <li>Oregon’s Bounty and OFB Buyer’s Guide</li>
+            <li>Scholarships, Young Farmers & Ranchers, and Women’s Advisory Council</li>
+            <li>Ag education and health & safety</li>
+            <li>Foundation for Education</li>
+            <li>Century Farm & Ranch</li>
+            <li>Summer Ag Institute</li>
+            <li>Farm Employer Education & Legal Defense Service</li>
+            <li>Resource Education & Agriculture Leadership</li>
+            <li>Oregon Agricultural Legal Foundation</li>
+        </ul>
+        <p>MCFB members also receive discounts on nearly 30 resources and services, such as new farm vehicles and equipment, solar panels, 
+            health insurance, and theft recovery. These benefits are constantly improving so members should check the 
+            <a href="https://oregonfb.org/member-discounts-2/">OFB website</a> to verify current offerings.</p>
+    </div>
+</div>
 <div class="container-fluid">
-    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
-    <p class="font-weight-bold">What benefits do MCFB member receive?</p>
-    <p>
-    In addition to our program benefits explained in <a href="benefits-our-approach.php">Our Approach</a>, MCFB members also receive all OFB statewide and AFBF nationwide benefits, which include access to, inclusion in, or influence over:
-    </p>
-    <ul>
-        <li>Oregon’s Bounty and OFB Buyer’s Guide</li>
-        <li>Scholarships, Young Farmers & Ranchers, and Women’s Advisory Council</li>
-        <li>Ag education and health & safety</li>
-        <li>Foundation for Education</li>
-        <li>Century Farm & Ranch</li>
-        <li>Summer Ag Institute</li>
-        <li>Farm Employer Education & Legal Defense Service</li>
-        <li>Resource Education & Agriculture Leadership</li>
-        <li>Oregon Agricultural Legal Foundation</li>
-    </ul>
-    <p>MCFB members also receive discounts on nearly 30 resources and services, such as new farm vehicles and equipment, solar panels, health insurance, and theft recovery. These benefits are constantly improving so members should check the <a href="https://oregonfb.org/member-discounts-2/">OFB website</a> to verify current offerings.</p>
     <p class="text-center">
-        <a href="index.php"   class="btn btn-light">&larr; Back</a>
+        <a href="index.php" class="btn btn-light">&larr; Back</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
+
 
 <?php include("footer.php"); ?>
--- join-us.php
+++ join-us.php
@@ -3,15 +3,20 @@
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
 
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
+        <p>
+        There are many ways to participate in and benefit from MCFB activities, these are explained more thoroughly in <a href="join-us-our-people.php">
+            Our People</a> and <a href="join-us-our-approach.php">Our Approach</a>. MCFB <a href="join-us-membership.php">Membership</a> can be attained 
+            by completing a hard copy membership packet, by completing the <a href="https://oregonfb.org/join/" target="blank">OFB's online application</a> 
+            or by signing up for OFB through <a href="https://www.countryfinancial.com/">Country Financial</a>. For more information, <a href="contact.php">Contact Us</a>.
+        </p>
+    </div>
+</div>
 <div class="container-fluid">
-    <h1 class="my-4 text-center"><?php echo $page_title; ?></h1>
-    <p>
-    There are many ways to participate in and benefit from MCFB activities -- these are explained more thoroughly in <a href="join-us-our-people.php">Our People</a> and <a href="join-us-our-approach.php">Our Approach</a>. MCFB <a href="join-us-membership.php">Membership</a> can be attained by completing a hard copy membership packet, by completing the
-        <a href="https://oregonfb.org/join/" target="blank">OFB's online application</a> or by signing up for OFB
-        through <a href="https://www.countryfinancial.com/">Country Financial</a>. For more information, <a href="contact.php">Contact Us</a>.
-    </p>
     <p class="text-center">
-        <a href="index.php"   class="btn btn-light">&larr; Back</a>
+        <a href="index.php" class="btn btn-light">&larr; Back</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
