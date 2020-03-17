dcd7b812ab6e774690d92227e1e569c4d135be2c
Marc Davies <marcdaviesriot@gmail.com>
2020-03-17 16:29:41 -0700

    add page intro

--- board.php
+++ board.php
@@ -4,11 +4,23 @@
 <div class="jumbotron">
     <div class="container" id="team">
         <h1 class="mb-4"><?php echo $page_title; ?></h1>
-        <div class= "row my-5">
-            <div class = "col-md-4 align-self-center">
+        <p>
+            MCFB's Board of Directors must include a President and Secretary as Board Officers, and no less than one additional Board 
+            Director. The Board of Directors may choose to elect a Vice-President and Treasurer as additional Board Officers, and up to 
+            eight additional Board Directors. In addition, if there is a county Women's Committee and/or a Young Farmers and Ranchers 
+            Committee, the Chair of each committee shall be a voting member of the Board of Directors.
+        </p>
+        <p>
+            Board Officers are elected by Voting Members each year at the Annual MCFB Meeting (see <a href="board-meetings.php" style="color: #007bff">Board Meetings</a>). 
+            Other Board Directors, also elected at the Annual MCFB Meeting, serve staggered terms of two years. Only full MCFB Voting Members 
+            can serve on the Board of Directors. In addition to the Annual MCFB Meeting, the Board of Directors normally meets seven times 
+            throughout each year (see <a href="board-meetings.php" style="color: #007bff">Board Meetings</a>).
+        </p>
+        <div class="row my-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Al.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>Al Garre - Board Director</h4>
                 <p> Al’s grandfather started the family farm in northeast Portland in 1920. Al took over the
                     operation in 1984. The family farm has always grown fresh market vegetables. Since Al
@@ -19,71 +31,70 @@
                     more out of the organization than he has given to it. It is Al’s firm belief that OFB and
                     MCFB do more for family farms than any other group. Al received his Bachelor of
                     Science in Mechanical Engineering in 1971 from Oregon State University.
-                </p> 
+                </p>
             </div>
         </div>
-        <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+        <div class="row mb-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Angi.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>Angi Bailey - Secretary</h4>
-                <p>Bio coming soon!</p> 
+                <p>Bio coming soon!</p>
             </div>
         </div>
-        <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+        <div class="row mb-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Deniece.jpg" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>Deniece Tucker - Treasurer</h4>
-                <p>Deniece was born and raised on her family’s farm located east of Troutdale near the Sandy River. The property was homesteaded in 1853 and has been farmed since 1952. Deniece and her family historically raised cattle and grew raspberries but converted to growing nursery stock in 2001 due to market changes. A member of the Multnomah County Farm Bureau since 1999, Deniece joined the Board of Directors in 2000 and has served as the Board President (2007-2008) and Vice-President (2009-2018). Deniece is currently serving as the Board Treasurer. Deniece holds an Associate Degree in Accounting, is an Oregon licensed tax consultant, and is a federally licensed tax agent.</p> 
+                <p>Deniece was born and raised on her family’s farm located east of Troutdale near the Sandy River. The property was homesteaded in 1853 and has been farmed since 1952. Deniece and her family historically raised cattle and grew raspberries but converted to growing nursery stock in 2001 due to market changes. A member of the Multnomah County Farm Bureau since 1999, Deniece joined the Board of Directors in 2000 and has served as the Board President (2007-2008) and Vice-President (2009-2018). Deniece is currently serving as the Board Treasurer. Deniece holds an Associate Degree in Accounting, is an Oregon licensed tax consultant, and is a federally licensed tax agent.</p>
             </div>
         </div>
-        <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+        <div class="row mb-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Helen.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>Helen Bushue - Board Director</h4>
-                <p>Helen and her husband, Barry, own and operate a small family farm east of Orient. Farm production includes greenhouse flowers, u-pick strawberries, and a fall pumpkin patch (with associated agritourism activities). Helen has been an MCFB member since 1988, served as the county Secretary and Treasurer for 25 years, and is now serving as a Board Director. Helen obtained a Bachelor of Education from Adelaide College of Advanced Education.</p> 
+                <p>Helen and her husband, Barry, own and operate a small family farm east of Orient. Farm production includes greenhouse flowers, u-pick strawberries, and a fall pumpkin patch (with associated agritourism activities). Helen has been an MCFB member since 1988, served as the county Secretary and Treasurer for 25 years, and is now serving as a Board Director. Helen obtained a Bachelor of Education from Adelaide College of Advanced Education.</p>
             </div>
         </div>
-        <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+        <div class="row mb-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Joe.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>Joe Rossi - Board Director</h4>
-                <p>Joe is a fourth-generation farmer in northeast Portland and currently farms alongside his daughter, Gabrielle Rossi. Joe and Gabrielle’s farm has always produced row crop fresh vegetables for sale and distribution in the Portland Metro area. As an urban farm, the location also avails itself to event space and farm tours. As a result, the farm is active in the community and the local Parkrose School District activities (the middle school and high school are built on two sections of the historic farm). Community and school engagement are traditions that Joe’s dad started and one that he continues today with Gabrielle. Being in an urban environment, the farm serves as a window into agriculture for our state. Joe and his family have been Multnomah County Farm Bureau members for more than 50 years. Joe became more involved when he joined the MCFB Board of Directors in 2018. Joe graduated from Parkrose High School and studied business at Portland State University before becoming a fulltime farmer.</p> 
+                <p>Joe is a fourth-generation farmer in northeast Portland and currently farms alongside his daughter, Gabrielle Rossi. Joe and Gabrielle’s farm has always produced row crop fresh vegetables for sale and distribution in the Portland Metro area. As an urban farm, the location also avails itself to event space and farm tours. As a result, the farm is active in the community and the local Parkrose School District activities (the middle school and high school are built on two sections of the historic farm). Community and school engagement are traditions that Joe’s dad started and one that he continues today with Gabrielle. Being in an urban environment, the farm serves as a window into agriculture for our state. Joe and his family have been Multnomah County Farm Bureau members for more than 50 years. Joe became more involved when he joined the MCFB Board of Directors in 2018. Joe graduated from Parkrose High School and studied business at Portland State University before becoming a fulltime farmer.</p>
             </div>
         </div>
-        <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+        <div class="row mb-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-John.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>John Bergan - Board Director</h4>
-                <p>John is a retired schoolteacher who joined the MCFB in 1990. When John was actively engaged on his farm east of Troutdale, he raised raspberries, cattle, hay, ornamental trees, and timber. John’s involvement with the MCFB Board begin in 2006. In 2007, he was elected as a Board Director and has continued his Board service to this day. John has served as an OFB delegate at the OFB annual convention.</p> 
+                <p>John is a retired schoolteacher who joined the MCFB in 1990. When John was actively engaged on his farm east of Troutdale, he raised raspberries, cattle, hay, ornamental trees, and timber. John’s involvement with the MCFB Board begin in 2006. In 2007, he was elected as a Board Director and has continued his Board service to this day. John has served as an OFB delegate at the OFB annual convention.</p>
             </div>
         </div>
-        <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+        <div class="row mb-5">
+            <div class="col-md-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Larry.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class="col-md-8 align-self-center">
                 <h4>Larry Bailey - President</h4>
-                <p>Bio coming soon!</p> 
+                <p>Bio coming soon!</p>
             </div>
         </div>
     </div>
 </div>
 <div class="container-fluid">
     <p class="text-center">
-        <a href="our-people.php"   class="btn btn-light">Back</a>
-        <a href="index.php"   class="btn btn-light">Home</a>
+        <a href="our-people.php" class="btn btn-light">Back</a>
+        <a href="index.php" class="btn btn-light">Home</a>
     </p>
     <?php include("statement.php"); ?>
 </div>
 <?php include("footer.php"); ?>
-
