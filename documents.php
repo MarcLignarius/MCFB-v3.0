da4b686e89f5a72d96865d561453c827bf83cbfc
Marc Davies <marcdaviesriot@gmail.com>
2020-03-18 14:36:17 -0700

    update documents.php

--- documents.php
+++ documents.php
@@ -5,15 +5,119 @@
     <div class="container">
         <h1 class="mb-4"><?php echo $page_title; ?></h1>
         <p>
-            MCFB's foundational, guiding, annual, and Board-related documents can be found here. If you are looking for an MCFB document 
+            MCFB's foundational, guiding, annual, and Board-related documents can be found here. If you are looking for an MCFB document
             that is not on this page, please <a href="contact.php" style="color:#007bff">Contact Us</a>.
         </p>
+        <p class="font-weight-bold">
+            Foundational documents:
+        </p>
         <ul>
-            <li><a href="articles-of-incorporation.php">Articles of Incorporation</a></li>
-            <li><a href="by-laws.php">By-laws</a></li>
-            <li><a href="business-plan.php">Business Plan</a></li>
-            <li><a href="annual-budgets.php">Annual Budgets</a></li>
-            <li><a href="board-meetings-agendas-and-minutes.php">Board Meetings Agendas & Minutes</a></li>
+            <li><a href="../files/MCFB-Articles of Incorporation-1958b.pdf" style="color:#007bff" download>
+                    MCFB Articles of Incorporation
+                </a>
+            </li>
+            <li>
+                <a href="../files/MCFB By-Laws-2005.pdf" style="color:#007bff" download>
+                    MCFB By-Laws
+                </a>
+            </li>
+            <li>
+                MCFB Business Plan (under preparation)
+            </li>
+        </ul>
+        <p class="font-weight-bold">
+            Policies:
+        </p>
+        <ul>
+            <li>
+                <a href="../files/OFB-Policy-Conflict of Interest-2010.pdf" style="color:#007bff" download>
+                    OFB Conflict of Interest
+                </a>
+            </li>
+            <li>
+                <a href="../files/OFB-Policy-Whistle Blower-2009.pdf" style="color:#007bff" download>
+                    OFB Whistle Blower
+                </a>
+            </li>
+        </ul>
+        <p class="font-weight-bold">
+            Annual documents:
+        </p>
+        <ul>
+            <li>
+                <a href="../files/MCFB FY19-20 Budget.pdf" style="color:#007bff" download>
+                    2019-2020 MCFB Budget
+                </a>
+            </li>
+        </ul>
+        <p class="font-weight-bold">
+            Board documents:
+        </p>
+        <ul>
+            <li>2020-03 MCFB Board Meeting Packet</li>
+            <li>
+                <a href="../files/MCFB BD Packet_2020_02_20.pdf" style="color:#007bff" download>
+                    2020-02 MCFB Board Meeting Packet
+                </a>
+            </li>
+            <li>
+                <a href="../files/MCFB BD Agenda_2020_01_14.pdf" style="color:#007bff" download>
+                    2020-01 MCFB Board Meeting Agenda
+                </a>
+            </li>
+            <ul>
+                <li>
+                    <a href="../files/MCFB BD Minutes_2020_01_14_FINAL.pdf" style="color:#007bff" download>
+                        Approved Minutes
+                    </a>
+                </li>
+            </ul>
+            <li>
+                <a href="../files/MCFB Annual Meeting Agenda_2019_11_08.pdf" style="color:#007bff" download>
+                    2019-11 MCFB Annual Meeting Agenda
+                </a>
+            </li>
+            <ul>
+                <li>
+                    <a href="../files/MCFB Annual Meeting Minutes_2019_11_08_FINAL.pdf" style="color:#007bff" download>
+                        Approved Minutes
+                    </a>
+                </li>
+            </ul>
+            <li>
+                <a href="../files/MCFB BD Agenda_2019_10_17.pdf" style="color:#007bff" download>
+                    2019-10 MCFB Board Meeting Agenda
+                </a>
+            </li>
+            <ul>
+                <li>
+                    <a href="../files/MCFB BD Minutes_2019_10_17_FINAL.pdf" style="color:#007bff" download>
+                        Approved Minutes
+                    </a>
+                </li>
+            </ul>
+            <li>
+                <a href="../files/MCFB BD Agenda_2019_09_26.pdf" style="color:#007bff" download>
+                    2019-09 MCFB Board Meeting Agenda
+                </a>
+            </li>
+            <ul>
+                <li>
+                    <a href="../files/MCFB BD Minutes_2019_09_26_FINAL.pdf" style="color:#007bff" download>
+                        Approved Minutes
+                    </a>
+                </li>
+            </ul>
+        </ul>
+        <p class="font-weight-bold">
+            Newsletters and mailers:
+        </p>
+        <ul style="list-style-type: none">
+            <li>
+                <a href="newsletters-and-mailers.php" style="color:#007bff">
+                    Click here
+                </a>
+            </li>
         </ul>
     </div>
 </div>
@@ -24,5 +128,4 @@
     </p>
     <?php include("statement.php"); ?>
 </div>
-
 <?php include("footer.php"); ?>
