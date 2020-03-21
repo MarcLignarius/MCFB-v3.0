aefb66f1c08e9fa2ee53849b88e42cfa277d4d65
Marc Davies <marcdaviesriot@gmail.com>
2020-03-21 15:29:40 -0700

    add banner

--- laws-and-regulations.php
+++ laws-and-regulations.php
@@ -1,9 +1,13 @@
 <?php $page_title = "Laws & Regulations"; ?>
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
+<div class="banner-container placeholder-container">
+    <div class="banner-text-container">
+        <h1 class="banner-title"><?php echo $page_title; ?></h1>
+    </div>
+</div>
 <div class="jumbotron">
     <div class="container">
-        <h1 class="mb-4"><?php echo $page_title; ?></h1>
         <p>
             There are numerous laws and regulations at the national, state, and local level that affect farming in Multnomah County. The links 
             below provide access to many, but not all of these. MCFB attempts to identify and become familiar with laws and regulations that 
