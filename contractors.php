06468bf9f367e763c7eeaa98197eefa45e49b69c
Marc Davies <marcdaviesriot@gmail.com>
2020-04-06 16:34:40 -0700

    refactor code to make use of new style values

--- contractors.php
+++ contractors.php
@@ -12,19 +12,19 @@
             MCFB contracts for services related to the executive director/secretary and treasurer.
         </p>
         <div class= "row my-5">
-            <div class = "col-md-4 align-self-center">
+            <div class = "col-lg-4 align-self-center">
                 <img src="../images/1.3.3.Bio-Jay.png" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class = "col-lg-8 align-self-center">
                 <h4>Jay Udelhoven - Executive Director</h4>
                 <p>Jay grew up in the Midwest, running around his grandparent's and uncle's dairy farms. Throughout his 30-year natural resource career, he has worked on numerous farm-related projects including dairy production in Africa, fish production in Panama, Mexico and Washington State, cattle ranching in Florida, and small-scale nursery/berry/mixed vegetable production in Oregon. Jay has served in senior level positions for several organizations over the past 20 years and has served as the MCFB Executive Director since July 2019. He holds a Bachelor of Science in Natural Resources from the University of Wisconsin-Madison and a Master of Environmental Policy from the University of Denver, Colorado.</p> 
             </div>
         </div>
         <div class= "row mb-5">
-            <div class = "col-md-4 align-self-center">
+            <div class = "col-lg-4 align-self-center">
                 <img src="../images/1.3.2.Bio-Deniece.jpg" alt="..." class="rounded-circle">
             </div>
-            <div class = "col-md-8 align-self-center">
+            <div class = "col-lg-8 align-self-center">
                 <h4>Deniece Tucker - Treasurer</h4>
                 <p>Deniece was born and raised on her family’s farm located east of Troutdale near the Sandy River. The property was homesteaded in 1853 and has been farmed since 1952. Deniece and her family historically raised cattle and grew raspberries but converted to growing nursery stock in 2001 due to market changes. A member of the Multnomah County Farm Bureau since 1999, Deniece joined the Board of Directors in 2000 and has served as the Board President (2007-2008) and Vice-President (2009-2018). Deniece is currently serving as the Board Treasurer. Deniece holds an Associate Degree in Accounting, is an Oregon licensed tax consultant, and is a federally licensed tax agent.</p> 
             </div>
