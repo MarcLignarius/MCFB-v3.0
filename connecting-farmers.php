058c27bd6bcbd7db4a7a8f3903fb283c59953cf6
Marc Davies <marcdaviesriot@gmail.com>
2020-04-06 17:21:38 -0700

    add thumbnails and banners for social outings page

--- connecting-farmers.php
+++ connecting-farmers.php
@@ -21,7 +21,7 @@
             <div class="card h-100 text-center">
                 <div class="image-text-container">
                     <a href="social-outings.php">
-                        <img class="card-image image-18">
+                        <img class="card-image image-20">
                         <h2 class="card-title">Social Outings</h2>
                     </a>
                 </div>
