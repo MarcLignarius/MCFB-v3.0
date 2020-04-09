0c9b3515996540f470ea217e95244d20abeb0504
Marc Davies <marcdaviesriot@gmail.com>
2020-04-08 21:18:46 -0700

    update benefits page

--- benefits.php
+++ benefits.php
@@ -8,25 +8,50 @@
 </div>
 <div class="jumbotron">
     <div class="container">
-        <p class="font-weight-bold">What benefits do MCFB member receive?</p>
+        <p class="font-weight-bold">What benefits do MCFB members receive?</p>
         <p>
-        In addition to our program benefits explained in <a href="benefits-our-approach.php">Our Approach</a>, MCFB members also receive all 
-        OFB statewide and AFBF nationwide benefits, which include access to, inclusion in, or influence over:
+            A <a href="https://oregonfb.org/about/ofb-country-financial/">longstanding relationship between Country Financial and the Farm Bureau</a> leads to significant savings for farmers through <a href="https://www.countryfinancial.com/">Country Financial</a> services. Because Country Financial understands farmers and farming, they are able to provide their financial and insurance services to farmers at rates that other insurers cannot match. <a href="https://www.countryfinancial.com/en/representative-locator.html">Find an agent</a> to discuss how you can save.
+        </p>
+        <p>
+        In addition to Country Financial benefits and our program benefits explained in <a href="benefits-our-approach.php">Our Approach</a>, MCFB members also receive all OFB statewide and AFBF nationwide benefits, which include access to, inclusion in, or influence over:
         </p>
         <ul>
-            <li>Oregon’s Bounty and OFB Buyer’s Guide</li>
-            <li>Scholarships, Young Farmers & Ranchers, and Women’s Advisory Council</li>
-            <li>Ag education and health & safety</li>
-            <li>Foundation for Education</li>
-            <li>Century Farm & Ranch</li>
-            <li>Summer Ag Institute</li>
-            <li>Farm Employer Education & Legal Defense Service</li>
-            <li>Resource Education & Agriculture Leadership</li>
-            <li>Oregon Agricultural Legal Foundation</li>
+            <li><a href="https://oregonfb.org/oregonsbounty/">Oregon’s Bounty</a> and <a href="https://www.orfarmersbuyersguide.com/">OFB Buyer’s Guide</a></li>
+            <li><a href="https://oregonfb.org/scholarships/">Scholarships</a>, <a href="https://oregonfb.org/programs/young-farmers-ranchers/">Young Farmers & Ranchers</a>, and <a href="https://oregonfb.org/programs/womens-advisory-council/">Women’s Advisory Council</a></li>
+            <li><a href="https://oregonfb.org/programs/ag_education/">Ag education</a> and <a href="https://oregonfb.org/safety/">health & safety</a></li>
+            <li><a href="https://oregonfb.org/programs/oregon-agriculture-educational-foundation-oaef/">Foundation for Education</a></li>
+            <li><a href="https://centuryfarm.oregonfb.org/">Century Farm & Ranch</a></li>
+            <li><a href="https://oregonfb.org/programs/summer-ag-institute/">Summer Ag Institute</a></li>
+            <li><a href="https://oregonfb.org/feelds/">Farm Employer Education & Legal Defense Service</a></li>
+            <li><a href="http://realoregon.net/">Resource Education & Agriculture Leadership</a></li>
+            <li><a href="https://oregonfb.org/programs/oregon-agricultural-legal-foundation/">Oregon Agricultural Legal Foundation</a></li>
         </ul>
-        <p>MCFB members also receive discounts on nearly 30 resources and services, such as new farm vehicles and equipment, solar panels, 
-            health insurance, and theft recovery. These benefits are constantly improving so members should check the 
-            <a href="https://oregonfb.org/member-discounts-2/">OFB website</a> to verify current offerings.
+        <p>MCFB members also receive discounts on nearly 30 resources and services, such as new farm vehicles and equipment, solar panels, health insurance, and theft recovery. The entire list of these benefits is below.
+        </p>
+        <ul>
+            <li>AgriPlan/BizPlan tax deduction program</li>
+            <li>Avis & Budget Rental Cars</li>
+            <li>Case-IH</li>
+            <li>Caterpillar Equipment</li>
+            <li>Choice Hotels</li>
+            <li>ClearValue Hearing</li>
+            <li>Earthlight Technologies</li>
+            <li>Farm Bureau Bank</li>
+            <li>Ford Bonus Cash</li>
+            <li>Grainger</li>
+            <li>Joe Hobson, Attorney at Law</li>
+            <li>John Deere</li>
+            <li>Life Flight Network</li>
+            <li>Life Line Screening</li>
+            <li>OFB Prescription Discount Program</li>
+            <li>OFB Theft Recovery Program</li>
+            <li>Pacific Building Systems</li>
+            <li>Supervised Fuels</li>
+            <li>TripBeat</li>
+            <li>Wyndham Hotels</li>
+        </ul>
+        <p>
+            The above benefits are constantly improving so members should check the <a href="https://oregonfb.org/member-discounts-2/">OFB website</a> to verify current offerings.
         </p>
     </div>
 </div>
