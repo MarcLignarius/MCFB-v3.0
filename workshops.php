262bcb3f67d0eadbbcc575150edae128b65af4da
Marc Davies <marcdaviesriot@gmail.com>
2020-04-21 12:20:10 -0700

    add banner for workshops page

--- workshops.php
+++ workshops.php
@@ -1,7 +1,7 @@
 <?php $page_title = "Workshops"; ?>
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
-<div class="banner-container" id="placeholder-container">
+<div class="banner-container" id="workshops-container">
     <div class="banner-text-container">
         <h1 class="banner-title"><?php echo $page_title; ?></h1>
     </div>
