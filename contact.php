b8b68927aae72b37ab3d92c01339457b17a2b992
Marc Davies <marcdaviesriot@gmail.com>
2020-04-20 20:47:25 -0700

    update image for contact page

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php $page_title = "Contact"; ?>
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
-<div class="banner-container contact-container">
+<div class="banner-container" id="contact-container">
     <div class="banner-text-container">
         <h1 class="banner-title"><?php echo $page_title; ?></h1>
     </div>
