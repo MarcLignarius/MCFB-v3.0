97e502379c150641306828fbec674d60ccd1b39b
Marc Davies <marcdaviesriot@gmail.com>
2020-04-21 12:18:48 -0700

    add banner for conferences page

--- conferences.php
+++ conferences.php
@@ -1,7 +1,7 @@
 <?php $page_title = "Conferences"; ?>
 <?php include("header.php"); ?>
 <?php include("navigation.php"); ?>
-<div class="banner-container" id="placeholder-container">
+<div class="banner-container" id="conferences-container">
     <div class="banner-text-container">
         <h1 class="banner-title"><?php echo $page_title; ?></h1>
     </div>
