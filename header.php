298c990544897e9d2f28981df45f7ed1bf1e20b7
Marc <37253788+MarcLignarius@users.noreply.github.com>
2020-04-23 12:33:42 -0700

    add http-equiv tag and update description

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="The Multnomah County Farm Bureau supports and helps local farmers be successful">
+    <meta name="description" content="We are non-profit grassroots organization working to enhance, protect, and advocate for agriculture and farmers in Multnomah County, Oregon.">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="author" content="">
     <title><?php echo $page_title; ?> | Multnomah County Farm Bureau</title>
     <!-- Bootstrap core CSS -->
